49dc307a50647a7b8e93db7314a12e584a24d53c
Arya <85712580+AryaSahay193@users.noreply.github.com>
2024-09-02 10:45:36 -0400

    Update state.cs

--- Scripts/state.cs
+++ Scripts/state.cs
@@ -2,28 +2,9 @@ using Godot;
 using System;
 
 public partial class State : StateMachine {
-	[Export] AnimatedSprite2D sprite; //Brings in all methods into this script of AnimatedSprite2D
-	[Export] Vector2 direction;
-	public StateMachine finiteStateMachine;
-	public virtual void EnterState() {
-		sprite.Play("Idle");
-	}
-	public virtual void ExitState() {
-	}
-	public virtual void UpdateState(float delta) {
-		if(direction.X != 0) {
-			//Transition to Move State.
-			//currentState = Move;
-		} if(Input.IsActionJustPressed("ui_jump")) {
-			//Transition to Jump State
-			//currentState = Jump;
-		} if(direction.X == 0) {
-			//Transition to Idle State.
-			//currentState = Idle;
-		}
-	}
-
-	public virtual void StartState() {}
-	public virtual void PhysicsUpdate(float delta) {}
-	public virtual void HandleInput(InputEvent @event) {}
+	public FiniteStateMachine fsm;
+	public virtual void EnterState(State previous = null) {}
+	public virtual void ExitState() {}
+	public virtual void Process(float delta) {}
+	public virtual void PhysicsProcess(float delta) {}
 }
