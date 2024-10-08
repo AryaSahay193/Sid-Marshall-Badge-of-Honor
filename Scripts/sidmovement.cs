4f15a844a1001b6c7b625c6df96f9bcce03fd76f
Arya <85712580+AryaSahay193@users.noreply.github.com>
2024-10-08 16:54:30 -0400

    Update sidmovement.cs
    (We're getting there) Walking and basic jumping mechanics work perfectly fine (except for the jump/fall animations in the Unity editor); run and double jumping needs working on, along with their animations

--- Scripts/sidmovement.cs
+++ Scripts/sidmovement.cs
@@ -1,66 +1,71 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class SidMovement : MonoBehaviour {
     //[SerializeField] allows you to directly edit a numerical value into Unity.
-    private float walkingSpeed = 3.5f;
-    private float runningSpeed = 7.0f;
-    private float jumpingSpeed = 20.0f;
-    private float numberOfJumps; //Number of jumps in the current state.
-    private float maximumJumps = 2; //The maximum number of jumps allowed.
-    private bool ground;
-    private bool doubleJump;
+    [SerializeField] private float walkingSpeed; //Edit walk speed in Unity.
+    [SerializeField] private float runningSpeed; //Edit run speed in Unity.
+    [SerializeField] private float jumpingSpeed; //Edit jump height in Unity.
+    [SerializeField] public LayerMask tileSetLayer; //Choose Layers in Unity.
+    private Vector2 rayCastSize = new Vector2(0.5f, 0.2f); //Creates the size of the BoxCast
+    private float rayCastDistance = 0.3f; //Determines how far the RayCast detects the ground.
+    private float horizontalMovement;
+    private bool grounded;
     
     //Reference variables
-    private Rigidbody2D sidMarshallRigidBody;
-    private Animator sidAnimations;
-    private InputActionReference sidButtonInputs;
+    [SerializeField] private Rigidbody2D sidMarshallRigidBody; //References the RigidBody2D class as a variable.
+    private Animator sidAnimations; //References the Animator class as a variable.
+    private BoxCollider2D boxCastCollision; //References the BoxCollider2D class as a variable.
 
     private void Awake() { //Everytime the script starts, the code runs with the Awake method.
         //Grabs references for the nodes in Unity.
         sidMarshallRigidBody = GetComponent<Rigidbody2D>();
         sidAnimations = GetComponent<Animator>();
+        boxCastCollision = GetComponent<BoxCollider2D>();
     }
 
-    private void Update() { //Code exacutes on every frame instead of starting immediately on awake.
-        float horizontalaxis = Input.GetAxis("Horizontal");
-        float verticalaxis = Input.GetAxis("Vertical");
-        sidMarshallRigidBody.velocity = new Vector2(horizontalaxis * walkingSpeed, sidMarshallRigidBody.velocity.y); //You can change the velocity of X, but Y stays the same.
-        if(Input.GetButton("Jump") && numberOfJumps > 0) {
-            Jump(); //Calls the method for jumping.
-            if(ground && numberOfJumps == 0) {
-                numberOfJumps = maximumJumps; //Resets the jump-counter back to 2.
-            }
-        }
-
-        if(horizontalaxis !=0 && Input.GetKey(KeyCode.LeftShift)) {
-            sidMarshallRigidBody.velocity = new Vector2(horizontalaxis * runningSpeed, sidMarshallRigidBody.velocity.y);
-        }
-
+    private void FixedUpdate() {
+        sidMarshallRigidBody.velocity = new Vector2(horizontalMovement * walkingSpeed, sidMarshallRigidBody.velocity.y); //You can change the velocity of X, but Y stays the same.
+        sidAnimations.SetFloat("Horizontal Velocity", Math.Abs(sidMarshallRigidBody.velocity.x));
+        sidAnimations.SetFloat("Vertical Velocity", sidMarshallRigidBody.velocity.y);
         //Flips the character sprite.
-        if(horizontalaxis > 0) {
-            transform.localScale = Vector3.one;
-        } else if(horizontalaxis < 0) {
-            transform.localScale = new Vector3(-1, 1, 1);
+        if(horizontalMovement > 0) {
+            transform.localScale = Vector3.one; //Keeps the vector the same.
+        } else if(horizontalMovement < 0) {
+            transform.localScale = new Vector3(-1, 1, 1); //Only changes the X-value of vector 3 (flips the character)
         }
-
-        //Animation Logic
-        sidAnimations.SetBool("Idle", horizontalaxis == 0);
-        sidAnimations.SetBool("Walk", horizontalaxis != 0);
-        sidAnimations.SetBool("Run", horizontalaxis != 0 && Input.GetKey(KeyCode.LeftShift));
-        sidAnimations.SetBool("Ground", ground); //Sets boolean for jumping.
     }
 
-    private void Jump() {
-        sidMarshallRigidBody.velocity = new Vector2(sidMarshallRigidBody.velocity.x, jumpingSpeed);
-        sidAnimations.SetTrigger("Jump");
-        ground = false; //When the player jumps, they are not on the ground anymore, which is why now it's false.
-        numberOfJumps -= 1;
+    public void Jump(InputAction.CallbackContext jump) {
+        sidAnimations.SetBool("Jump", !grounded);
+        if(jump.performed && groundChecker()) {
+            sidAnimations.SetTrigger("Jump");
+            sidMarshallRigidBody.velocity = new Vector2(sidMarshallRigidBody.velocity.x, jumpingSpeed);
+        } if(jump.canceled && sidMarshallRigidBody.velocity.y > 0.0f) {
+            sidMarshallRigidBody.velocity = new Vector2(sidMarshallRigidBody.velocity.x, sidMarshallRigidBody.velocity.y * 0.5f);
+        }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
-        if(collision.gameObject.tag == "Ground") {
-            ground = true; //Ensures that the player is on the ground.
+    public void Movement(InputAction.CallbackContext movement) {
+        horizontalMovement = movement.ReadValue<Vector2>().x; //Reads on the x-values
+    }
+
+    public void Accelerate(InputAction.CallbackContext accelerate) {
+        if(accelerate.performed && horizontalMovement != 0) {
+            sidMarshallRigidBody.velocity = new Vector2(runningSpeed, sidMarshallRigidBody.velocity.y);
+            sidAnimations.SetBool("Run", horizontalMovement != 0 && accelerate.performed);
         }
+    }
+
+    public bool groundChecker() {
+        RaycastHit2D collisionDetection = Physics2D.BoxCast(boxCastCollision.bounds.center, boxCastCollision.bounds.size, 0.0f, Vector2.down, 0.1f, tileSetLayer);
+        return collisionDetection.collider != null;
+    }
+
+    public bool wallChecker() {
+        Vector2 wallDirection = new Vector2(transform.localScale.x, 0);
+        RaycastHit2D collisionDetection = Physics2D.BoxCast(boxCastCollision.bounds.center, boxCastCollision.bounds.size, 0.0f, wallDirection, 0.1f, tileSetLayer);
+        return collisionDetection.collider != null;
     }
 }
