297a16ebc5701981b696507cf8161158484c71a5
Arya <85712580+AryaSahay193@users.noreply.github.com>
2025-04-02 08:50:32 +0530

    Update sidmovement.cs
    Movement does not work horizontally mainly because of the Unity Input System, and I am also trying to split animation logic and movement logic into two scripts (redoing the Unity Animation State Machine)

--- Unity/Scripts/sidmovement.cs
+++ Unity/Scripts/sidmovement.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Interactions;
 
-public class Player_Controller : MonoBehaviour, PlayerController.IPlayerActions {
+public class Player_Movement : MonoBehaviour, PlayerController.IPlayerActions {
+    //All public variables can be accessed to other scripts. Private variables are only able to be changed within this script.
     [Header("Reference Variables")]
     [SerializeField] private Rigidbody2D sidMarshallRigidBody; //References the RigidBody2D class as a variable.
     [SerializeField] private PhysicsMaterial2D physicsMaterial;
     [SerializeField] private ParticleSystem dustParticles;
     [SerializeField] private Transform groundCollider;
     [SerializeField] private Transform wallCollider;
-    [SerializeField] private Transform ceilingCollider;
     [SerializeField] private LayerMask tileSetLayer; //Choose Layers in Unity.
-    private Animator sidAnimations; //References the Animator class as a variable.
     private BoxCollider2D boxCastCollider; //References the BoxCollider2D class as a variable.
     private Vector2 boxCastSize; //Creates a Vector2 for a Box Cast Size.
     private bool faceDirectionRight = true; //By default
@@ -25,12 +24,12 @@ public class Player_Controller : MonoBehaviour, PlayerController.IPlayerActions 
     private bool isOnTheSlope;
     
     [Header("Movement Variables")]
-    private float currentSpeed, currentHeight;
-    [SerializeField] private float walkingSpeed;
+    public float currentSpeed, currentHeight;
     [SerializeField] private float acceleration;
     [SerializeField] private float friction;
+    [SerializeField] public float walkingSpeed;
+    [SerializeField] public float runningSpeed;
     [SerializeField] private float wallAcceleration;
-    [SerializeField] private float runningSpeed;
     [SerializeField] private float jumpingSpeed;
     [SerializeField] private float slidingSpeed;
     [SerializeField] private float wallSlidingSpeed;
@@ -43,20 +42,18 @@ public class Player_Controller : MonoBehaviour, PlayerController.IPlayerActions 
     [SerializeField] private float wallJumpTime;
     private float coyoteCounter, jumpBufferCounter, wallJumpCounter, wallJumpDirection, wallJumpDuration, divePower, diveCooldown;
     private bool groundSliding, wallSliding, wallJumping, previouslyGrounded, canDive = true, isDiving;
-    private int jumpsLeft, maximumJumps = 2;
+    public int jumpsLeft, maximumJumps = 2;
     
     [Header("Input-System References")]
     private PlayerController playerControls;
-    private bool doublePress, attackingAction, crouchingAction, doorEnterAction, confirmButton, cancelButton;
-    private float directionalInput, runningAction, jumpingAction, tapDelay, pressPoint;
-    private int tapAmount = 2;
+    public bool doublePress, attackingAction, crouchingAction, divingAction, doorEnterAction, confirmButton, cancelButton;
+    public float directionalInput, runningAction, jumpingAction;
 
     private void Awake() { //Everytime the script starts, the code runs with the Awake method.
         //Grabs references for the nodes in Unity.
         playerControls = new PlayerController();
         playerControls.Player.SetCallbacks(this); //Initializes all control schemes and phases.
         sidMarshallRigidBody = GetComponent<Rigidbody2D>(); //Initializes the RigidBody component
-        sidAnimations = GetComponent<Animator>(); //Initializes the Animator component
         boxCastCollider = GetComponent<BoxCollider2D>(); //Initializes the BoxCollider2D component.
         boxCastSize = boxCastCollider.size; //Sets the size for the BoxCollider2D for slope-checks.
         jumpsLeft = maximumJumps; //Resets the amount of jumps at the start of the game.
@@ -92,6 +89,10 @@ public class Player_Controller : MonoBehaviour, PlayerController.IPlayerActions 
         crouchingAction = crouching.ReadValue<bool>(); //Single button.
     }
 
+    public void OnDive(InputAction.CallbackContext diving) {
+        divingAction = diving.ReadValue<bool>(); //Single button.
+    }
+
     //Assigns the attack button to the jump mechanic.
     public void OnAttack(InputAction.CallbackContext attacking) {
         attackingAction = attacking.ReadValue<bool>(); //Single button.
@@ -112,64 +113,54 @@ public class Player_Controller : MonoBehaviour, PlayerController.IPlayerActions 
         cancelButton = cancel.ReadValue<bool>(); //Single button.
     }
 
+    public void Process(ref InputInteractionContext multiTap) {
+        var controls = multiTap.control;
+        if(inputControls != null) {
+            if(inputControls != controls) return;
+            var isStillActuated = multiTap.ControlIsActuated(0.75f);
+            var actuationTime = multiTap.time - multiTap.startTime;
+            if(!isStillActuated) {
+                if(actuationTime >= 1) multiTap.Performed();
+                else multiTap.Canceled();
+            }
+        } else {
+            var isActuated = multiTap.ControlIsActuated(0.75f);
+            if(isActuated) {
+                inputControls = multiTap.control;
+                multiTap.Started();
+            }
+        }
+    }
+
+    InputControl inputControls;
+    public void Reset() {
+        inputControls = null;
+    }
+
     //MAIN METHOD: Handles code that runs every frame, such as Animation Logic.
-    private void Update() {
+    public void Update() {
         currentSpeed = sidMarshallRigidBody.velocity.x;
         currentHeight = sidMarshallRigidBody.velocity.y;
         previouslyGrounded = characterOnGround();
-        /*var ButtonCooldown = 0.5f;
-        var ButtonCount = 0;
-        var tapAmount = 3;
-        //Multi-Tap Detection
-        if(Keyboard.current.anyKey.IsPressed()) {
-            if(ButtonCooldown > 0 && ButtonCount == 1/(tapAmount - 1)) {
-                return;
-            } else {
-                ButtonCooldown = 0.5f;
-                ButtonCount += 1;
-            }
-        } 
-        if(ButtonCooldown > 0) ButtonCooldown -= 1 * Time.deltaTime;
-        else ButtonCount = 0;*/
         
         //Basic Movement: Walking and Running (With animation-logic)        
         flipCharacter();
         if(isDiving) return;
         if(!wallJumping) {
             if(characterOnGround()) {
-                coyoteCounter = coyoteTime; //Resets the CoyoteCounter. 
-                sidAnimations.SetBool("Walled", false);
-                sidAnimations.SetBool("Grounded", true);
                 performMovement(); //Performs basic movements.
                 performCrouchSlide(); //Performs crouch/slide movements.
                 characterOnSlope(groundRaySize); //Performs slope movements.
+                coyoteCounter = coyoteTime; //Resets the CoyoteCounter. 
                 jumpsLeft = maximumJumps; //Resets the number of jumps.
-            } else {
-                coyoteCounter -= Time.deltaTime;
-                if(currentHeight >= jumpingSpeed || currentHeight < 0) {
-                    sidAnimations.SetBool("Grounded", false);
-                    sidAnimations.SetFloat("Vertical Velocity", -1.0f); //Falling Logic
-                }
-            }
-        }
+            } else coyoteCounter -= Time.deltaTime;
+        } if(divingAction) StartCoroutine(Dive());
 
-        /*if(characterOnGround() && double-tap) {
-            StartCoroutine(Dive());
-        }*/
-        
-        if(!characterOnGround() && characterOnwall()) {
-            sidAnimations.SetBool("Walled", true);
-            sidAnimations.SetFloat("Vertical Velocity", 2.0f); //Wall-Contact Animation
-            performWallJump();
-        } else {
-            sidAnimations.SetBool("Walled", false);
-        }
-
+        //Jump-Buffer and Jump-Conditions
         if(jumpingAction > 0.0f) {
             jumpBufferCounter = jumpBufferTime; //Resets the jump-buffer.
         } else {
             jumpBufferCounter -= Time.deltaTime;
-            Debug.Log("Jump Buffer: " + jumpBufferCounter);
         } if(coyoteCounter > 0.0f && jumpBufferCounter >= 0) {
             jumpBufferCounter = 0.0f;
             jumpsLeft -= 1;
@@ -177,6 +168,20 @@ public class Player_Controller : MonoBehaviour, PlayerController.IPlayerActions 
         } if(jumpingAction == 0.0f && currentHeight > 0) {
             sidMarshallRigidBody.velocity = new Vector2(currentSpeed, currentHeight * 0.5f); //Variable Jump Height by length of button press.
             coyoteCounter = 0.0f;
+        }
+
+        //Course-correction while falling.
+        float horizontalFallSpeed = 5.0f;
+        float pointOfFallAdjustment = -3.0f;
+        float midairCourseCorrection = horizontalFallSpeed * directionalInput;
+        if(!characterOnGround() && currentHeight <= pointOfFallAdjustment && currentSpeed <= runningSpeed) {
+            if(directionalInput != 0.0f) {
+                float midairSpeed = Mathf.MoveTowards(currentSpeed, midairCourseCorrection, acceleration * Time.deltaTime * 10.0f);
+                sidMarshallRigidBody.velocity = new Vector2(midairSpeed, currentHeight);
+            } else if(directionalInput == 0.0f || directionalInput != Mathf.Abs(directionalInput)) {
+                float zeroFallMovement = Mathf.MoveTowards(currentSpeed, 0.0f, (friction * Time.deltaTime * 2.5f));
+                sidMarshallRigidBody.velocity = new Vector2(zeroFallMovement, currentHeight);
+            }
         }
     }
 
@@ -193,7 +198,6 @@ public class Player_Controller : MonoBehaviour, PlayerController.IPlayerActions 
         //Directional-Changes (Skiddding and Dust Trails).
         float previousDirection = directionalInput;
         if((characterOnGround() || previouslyGrounded) && previousDirection != Mathf.Sign(directionalInput) && (currentSpeed > walkingSpeed || currentSpeed < -walkingSpeed)) {
-            sidAnimations.Play("Skid");
             dustParticles.Play();
         }
     }
@@ -205,7 +209,6 @@ public class Player_Controller : MonoBehaviour, PlayerController.IPlayerActions 
         float originalGravity = sidMarshallRigidBody.gravityScale;
         sidMarshallRigidBody.gravityScale = 0.0f; //Gravity is non-existent while diving.
         sidMarshallRigidBody.velocity = new Vector2(transform.localScale.x * divePower, 0.0f);
-        sidAnimations.Play("Dive Roll");
         yield return new WaitForSeconds(diveTime);
         sidMarshallRigidBody.gravityScale = originalGravity; //Resets gravity.
         isDiving = false;
@@ -214,14 +217,10 @@ public class Player_Controller : MonoBehaviour, PlayerController.IPlayerActions 
     }
     
     private void performMovement() {
-        if(directionalInput == 0) {
-            sidAnimations.SetFloat("Horizontal Velocity", 0.0f); //Idle Animation Logic
-        } if(directionalInput != 0) { //This if-else statement is basically calculating the velocity which we can then substitute in our RigidBody2D component.
+        if(directionalInput != 0) { //This if-else statement is basically calculating the velocity which we can then substitute in our RigidBody2D component.
             currentSpeed = Mathf.MoveTowards(sidMarshallRigidBody.velocity.x, walkingSpeed * directionalInput, acceleration * Time.deltaTime * 10.0f);
-            sidAnimations.SetFloat("Horizontal Velocity", 1.0f); //Walking Animation Logic
             if(runningAction != 0 && currentSpeed < runningSpeed) {
                 currentSpeed = Mathf.MoveTowards(sidMarshallRigidBody.velocity.x, runningSpeed * directionalInput, acceleration * Time.deltaTime * 10.0f);
-                sidAnimations.SetFloat("Horizontal Velocity", 2.0f); //Running Animation Logic
             }
         } else if(directionalInput == 0 || (directionalInput == 0 && runningAction == 0)) {
             currentSpeed = Mathf.MoveTowards(sidMarshallRigidBody.velocity.x, 0.0f, friction * Time.deltaTime * 10.0f);
@@ -239,27 +238,16 @@ public class Player_Controller : MonoBehaviour, PlayerController.IPlayerActions 
         if(coyoteCounter <= 0.0f && !characterOnwall() && jumpsLeft == 0) return;
         if(characterOnGround() || coyoteCounter > 0.0f || jumpsLeft >= 0) {
             sidMarshallRigidBody.velocity = new Vector2(currentSpeed, jumpingSpeed);
-            sidAnimations.SetBool("Grounded", false);
-
-            if(jumpsLeft == 1) sidAnimations.SetFloat("Vertical Velocity", 0.0f); //Single Jump Logic
-            else if(jumpsLeft == 0) sidAnimations.SetFloat("Vertical Velocity", 1.0f); //Double Jump Logic
         } if(!characterOnGround() && sidMarshallRigidBody.velocity.y < 0.0f && directionalInput != 0) {
             float midairCourseCorrection = walkingSpeed * directionalInput;
             sidMarshallRigidBody.velocity = new Vector2(midairCourseCorrection, sidMarshallRigidBody.gravityScale);
         }
     }
 
-    //Method that detects if a key press is double-pressed or not
-    private void doubleKeyPress(Keyboard buttonInput) {
-        /*if(/*buttonInput pressed twice) {
-        }*/
-    }
-
     //Method that deals with wall-sliding and wall-jumping.
     private void performWallJump() {
         if(characterOnwall() && !characterOnGround() && directionalInput != 0) {
             sidMarshallRigidBody.velocity = new Vector2(sidMarshallRigidBody.velocity.x, Mathf.Clamp(sidMarshallRigidBody.velocity.y, -wallSlidingSpeed, float.MaxValue));
-            sidAnimations.SetFloat("Vertical Velocity", 3.0f); //Wall-Sliding Animation
             wallSliding = true;
         } else {
             wallSliding = false;
@@ -276,7 +264,6 @@ public class Player_Controller : MonoBehaviour, PlayerController.IPlayerActions 
         
         if(jumpingAction > 0.0f && wallJumpCounter > 0.0f) {
             sidMarshallRigidBody.velocity = new Vector2(wallJumpDirection * wallJumpSpeed.x, wallJumpSpeed.y);
-            sidAnimations.SetFloat("Vertical Velocity", 4.0f); //Wall-Kick Animation
             wallJumpCounter = 0.0f;
             wallJumping = true;
             if(transform.localScale.x != wallJumpDirection) {
@@ -298,27 +285,22 @@ public class Player_Controller : MonoBehaviour, PlayerController.IPlayerActions 
         Vector2 originalBoxCastSize = boxCastSize;
         if(crouchingAction && characterOnGround()) {
             boxCastSize = new Vector2(boxCastCollider.size.x, boxCastCollider.size.y / 2);
-            sidAnimations.Play("Crouch");
             if(currentSpeed == runningSpeed) {
                 groundSliding = true;
                 currentSpeed = Mathf.MoveTowards(slidingSpeed, 0.0f, friction * Time.deltaTime * 10.0f);
                 boxCastSize = new Vector2(3.5f, 1.5f);
-                sidAnimations.Play("Slide");
             } if(groundSliding && currentSpeed >= 0.01f && jumpingAction > 0) {
                 groundSliding = false;
-                sidAnimations.Play("Slide Recover");
             }
         } else if(!crouchingAction) {
             groundSliding = false;
             boxCastSize = originalBoxCastSize;
-            sidAnimations.Play("Crouch Recover");
         }
     }
 
     //Method that deals with attacks.
     private void performAttack() {
         if(characterOnGround() && attackingAction && (currentSpeed <= walkingSpeed || currentSpeed >= -walkingSpeed)) {
-            sidAnimations.Play("Kick (Stationary)");
             sidMarshallRigidBody.velocity = new Vector2(0.0f, 0.0f);
         }
     }
