daa3540606a8e013934f8564dbe7f4c441f33ef5
Arya <85712580+AryaSahay193@users.noreply.github.com>
2025-04-10 17:54:02 +0530

    Update playermovement.cs
    Working character movement, flipping of sprites and basic acceleration/friction. More features will be coming soon

--- Redot/Scripts/playermovement.cs
+++ Redot/Scripts/playermovement.cs
@@ -1,30 +1,59 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
 public partial class playermovement : CharacterBody2D {
 	private int maximumJumps = 2, jumpsLeft;
-	private const float jumpVelocity = -265.0f;
-	private const float walkingSpeed = 79.5f, runningSpeed = 159.0f;
-	private float acceleration, friction;
+	private float jumpVelocity = -265.0f;
+	private float walkingSpeed = 53.0f, runningSpeed = 159.0f;
+	private float acceleration = 3.0f, friction = 5.0f, currentSpeed;
 	private float wallJumpSpeed, wallSlideSpeed;
+	private Vector2 direction, velocity;
+	private AnimatedSprite2D playerAnimations;
 
     public override void _Ready() {
-        jumpsLeft = maximumJumps;
+        playerAnimations = GetNode<AnimatedSprite2D>("Animations");
+		jumpsLeft = maximumJumps;
     }
 
     public override void _PhysicsProcess(double delta) {
-		Vector2 direction = Input.GetVector("player_left", "player_right", "player_up", "player_down");
-		Vector2 velocity = Velocity;
+		direction = Input.GetVector("player_left", "player_right", "player_up", "player_down");
+		velocity = Velocity;
 
 		//Jump and Gravity.
 		if (!IsOnFloor()) velocity += GetGravity() * (float)delta; //Gravity.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor()) velocity.Y = jumpVelocity; //Jump.
+		if (Input.IsActionJustPressed("player_jump") && IsOnFloor()) {
+			velocity.Y = jumpVelocity;
+			if(direction.Y > 0.0f) playerAnimations.Play("Jump");
+			else playerAnimations.Play("Fall");
+			if(IsOnFloor()) playerAnimations.Play("Land");
+		}
 
 		//Walking movements.
-		if (direction != Vector2.Zero) velocity.X = direction.X * walkingSpeed;
-		else velocity.X = Mathf.MoveToward(Velocity.X, 0, walkingSpeed);
+		if(IsOnFloor()) {
+			currentSpeed = direction.X * walkingSpeed;
+			if(direction != Vector2.Zero) {
+				if(Input.IsActionPressed("player_run")) {
+					currentSpeed = direction.X * runningSpeed;
+					velocity.X = Mathf.MoveToward(velocity.X, currentSpeed, acceleration);
+					playerAnimations.Play("Run");
+				} else {
+					velocity.X = Mathf.MoveToward(velocity.X, currentSpeed, acceleration);
+					playerAnimations.Play("Walk");
+				}
+			} else { 
+				velocity.X = Mathf.MoveToward(velocity.X, 0.0f, friction);
+				playerAnimations.Play("Idle");
+			}
+		}
 
+		flipCharacter(direction);
 		Velocity = velocity;
 		MoveAndSlide();
+	}
+
+	private void flipCharacter(Vector2 direction) {
+		if(direction.X < 0) playerAnimations.FlipH = true;
+		if(direction.X > 0) playerAnimations.FlipH = false;
 	}
 }
