58aaa7be87f25a4215dec6f989c84d1dddd0b13b
Arya <85712580+AryaSahay193@users.noreply.github.com>
2025-04-12 16:53:52 +0530

    Update playermovement.cs
    I added double jump, but my animations are not working because I want to change from AnimatedSprite2D to AnimationPlayer/AnimationTree

--- Redot/Scripts/playermovement.cs
+++ Redot/Scripts/playermovement.cs
@@ -3,57 +3,109 @@ using System;
 using System.Diagnostics;
 
 public partial class playermovement : CharacterBody2D {
-	private int maximumJumps = 2, jumpsLeft;
-	private float jumpVelocity = -265.0f;
+	private bool isIdle, isMoving, isRunning, isAttacking, isCrouching, isSliding, isDiving = false;
+	private float acceleration = 3.0f, friction = 5.0f, currentVelocity;
+	private float jumpVelocity = -106.0f, gravityValue = 291.5f;
 	private float walkingSpeed = 53.0f, runningSpeed = 159.0f;
-	private float acceleration = 3.0f, friction = 5.0f, currentSpeed;
 	private float wallJumpSpeed, wallSlideSpeed;
+	private int maximumJumps = 2, numberOfJumps = 0;
+	private CollisionShape2D playerCollider;
+	private AnimationPlayer animationPlayer;
+	private AnimationTree playerAnimator;
 	private Vector2 direction, velocity;
-	private AnimatedSprite2D playerAnimations;
+	private Sprite2D playerSprites;
+	private Variant blendSpaceAmount = new int();
 
     public override void _Ready() {
-        playerAnimations = GetNode<AnimatedSprite2D>("Animations");
-		jumpsLeft = maximumJumps;
+		playerSprites = GetNode<Sprite2D>("Main Sprites");
+		animationPlayer = GetNode<AnimationPlayer>("Main Sprites/AnimationTree"); //Grabs the Animation Player reference
+        playerAnimator = GetNode<AnimationTree>("Main Sprites/AnimationTree"); //Grabs the Animation Tree reference.
+		playerAnimator.Set("parameters/conditions/isMoving", blendSpaceAmount); //Assigns the parameter as a Boolean.
+		playerAnimator.Get("parameters/playback");
+		playerAnimator.Active = true; //Sets the Animation Tree active at the start.
+		numberOfJumps = maximumJumps;
     }
 
     public override void _PhysicsProcess(double delta) {
 		direction = Input.GetVector("player_left", "player_right", "player_up", "player_down");
 		velocity = Velocity;
 
-		//Jump and Gravity.
-		if (!IsOnFloor()) velocity += GetGravity() * (float)delta; //Gravity.
-		if (Input.IsActionJustPressed("player_jump") && IsOnFloor()) {
-			velocity.Y = jumpVelocity;
-			if(direction.Y > 0.0f) playerAnimations.Play("Jump");
-			else playerAnimations.Play("Fall");
-			if(IsOnFloor()) playerAnimations.Play("Land");
-		}
-
-		//Walking movements.
+		//Handles animations and movement.
+		handlePlayerSprites(direction); //Handles flipping sprites and animations.
 		if(IsOnFloor()) {
-			currentSpeed = direction.X * walkingSpeed;
-			if(direction != Vector2.Zero) {
-				if(Input.IsActionPressed("player_run")) {
-					currentSpeed = direction.X * runningSpeed;
-					velocity.X = Mathf.MoveToward(velocity.X, currentSpeed, acceleration);
-					playerAnimations.Play("Run");
-				} else {
-					velocity.X = Mathf.MoveToward(velocity.X, currentSpeed, acceleration);
-					playerAnimations.Play("Walk");
-				}
-			} else { 
-				velocity.X = Mathf.MoveToward(velocity.X, 0.0f, friction);
-				playerAnimations.Play("Idle");
-			}
+			performMovement();
+			performCrouchandSlide();
 		}
 
-		flipCharacter(direction);
+		if(!IsOnFloor()) velocity.Y += gravityValue * (float)delta; //Gravity.
+		if(velocity.Y < 0.0f) {} //Play "Jump" Animation
+		else if(velocity.Y > 0.0f) {
+			//Play "Jump - Fall" Animation
+			if(IsOnFloor()) {} //Play "Jump - Land" Animation
+		}
+
+		//performAttack();
+		performJump();
 		Velocity = velocity;
 		MoveAndSlide();
 	}
+	
+	private void handlePlayerSprites(Vector2 direction) {
+		if(direction.X < 0.0f) playerSprites.FlipH = true;
+		else if(direction.X > 0.0f) playerSprites.FlipH = false;
+		
+		if(IsOnFloor()) {
+			if(direction.X == 0.0f) {
+				isIdle = true;
+				isMoving = false;
+				isRunning = false;
+			} else if(direction.X != 0.0f) {
+				isIdle = false;
+				isMoving = true;
+				isRunning = false;
+			} else if(isMoving && Input.IsActionPressed("player_run")) {
+				isIdle = false;
+				isRunning = true;
+			}
+		}
+	}
 
-	private void flipCharacter(Vector2 direction) {
-		if(direction.X < 0) playerAnimations.FlipH = true;
-		if(direction.X > 0) playerAnimations.FlipH = false;
+	private void performMovement() {
+		float runningAcceleration = 6.0f;
+		currentVelocity = direction.X * walkingSpeed;
+		if(direction != Vector2.Zero) {
+			if(Input.IsActionPressed("player_run")) {
+				currentVelocity = direction.X * runningSpeed;
+				velocity.X = Mathf.MoveToward(velocity.X, currentVelocity, runningAcceleration);
+				//Play "Run" Animation
+			} else {
+				velocity.X = Mathf.MoveToward(velocity.X, currentVelocity, acceleration);
+				//Play "Walk" Animation
+			}
+		} else { 
+			velocity.X = Mathf.MoveToward(velocity.X, 0.0f, friction);
+			//Play "Skidding" Animation
+		}
+	}
+
+	private void performJump() {
+		if(Input.IsActionJustPressed("player_jump") && numberOfJumps < maximumJumps) {
+			velocity.Y = jumpVelocity;
+			numberOfJumps += 1;
+		} 
+		if(IsOnFloor()) numberOfJumps = 0;
+		else if(numberOfJumps == 0) numberOfJumps += 1;
+	}
+
+	private void performAttack() {
+		if(isAttacking) {
+			velocity.X = 0.0f;
+			//Play "Kick - Normal"
+			playerSprites.FlipH = false;
+		} 
+	}
+
+	private void performCrouchandSlide() {
+		if(Input.IsActionPressed("player_down")) {}
 	}
 }
