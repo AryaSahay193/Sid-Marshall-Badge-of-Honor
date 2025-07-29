8fd1f8998b09badbe9fc3b90f4bd8401d8b81434
Arya <85712580+AryaSahay193@users.noreply.github.com>
2025-07-29 23:38:43 +0530

    Update PlayerScript.cs
    I moved all variables that used to be in BaseStateClass in this script so this way it would be less cluttered.

--- Redot/Scripts/Player/State Machine/PlayerScript.cs
+++ Redot/Scripts/Player/State Machine/PlayerScript.cs
@@ -2,21 +2,34 @@ using Godot;
 using System;
 
 public partial class PlayerScript : CharacterBody2D {
-	public float currentHealth, maximumHealth = 100.00f;
+	public bool runButton, crouchButton, kickButton, punchButton, jumpButton, inBattleMode;
+	public float currentHealth, maximumHealth = 100.00f, gravityValue = 312.7f;
 	public TileMapLayer grassyTerrain, hardFloorTerrain;
 	public Vector2 characterVelocity, moveDirection;
-	
-	public bool isGrounded, isWalled, isRoofed;
-	public float currentVelocity, gravityValue = 312.7f;
+	[Export] public AnimatedSprite2D playerAnimations;
+	[Export] public CollisionShape2D playerCollider;
 	
 	public override void _Ready() {
 		moveDirection = Input.GetVector("player_left", "player_right", "player_up", "player_down");
+		punchButton = Input.IsActionJustPressed("player_punch");
+		kickButton = Input.IsActionJustPressed("player_kick");
+		jumpButton = Input.IsActionJustPressed("player_jump");
+		crouchButton = Input.IsActionPressed("player_down");
+		runButton = Input.IsActionPressed("player_run");
 		currentHealth = maximumHealth;
 	}
-
+	
+	//Function that handles physics and time-related code.
 	public override void _PhysicsProcess(double delta) {
-		characterVelocity.Y += gravityValue * (float)delta; //Force of Gravity will always be acting on the player.
-		characterVelocity = Velocity; //Setting the Velocity Vector2 equal to velocity.
-		MoveAndSlide(); //A necessary movement to make movement in Redot engine work.
+		characterVelocity = Velocity;
+		characterVelocity.Y += gravityValue;
+		MoveAndSlide();
+	}
+
+	public void flipCharacter(float direction) {
+		if(direction != 0.0f) {
+			if(direction < 0.0f) playerAnimations.FlipH = true;
+			else if(direction > 0.0f) playerAnimations.FlipH = false;
+		}
 	}
 }
