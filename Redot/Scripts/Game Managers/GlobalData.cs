288b6c1f9fee8dbfdf9ff2697c219b82f3be5098
Arya <85712580+AryaSahay193@users.noreply.github.com>
2025-09-21 21:26:54 -0400

    Update GlobalData.cs
    Adding more things to the singleton script, for ease of access throughout the project.

--- Redot/Scripts/Game Managers/GlobalData.cs
+++ Redot/Scripts/Game Managers/GlobalData.cs
@@ -6,16 +6,24 @@ public sealed partial class GlobalData : Node {
 	public float playerHealth, maximumHealth = 100.0f; //Health will carry over through the entire game.
 	public int jigsawCounter = 0, documentCounter = 0; //Collectibles will carry over throughout the game.
 	public Camera2D playerCameraReference; //Only one player-camera instance.
-	public SidMarshall playerReference; //Only one player instance.
+	public PlayerController playerReference; //Only one player instance.
 	public ProgressBar playerHealthBar; //One player-health isntance.
-	
-	public override void _Ready() {
-		playerCameraReference = GetNode<Camera2D>("/root/GameWorld/SidMarshall/PlayerCamera");
-		playerHealthBar = GetNode<ProgressBar>("/root/GameWorld/UIElements/PlayerUI/Health");
-		playerReference = GetNode<SidMarshall>("/root/GameWorld/SidMarshall");
-		maximumHealth = playerReference.maximumHealth;
-		playerHealth = playerReference.currentHealth; 
+	public InputManager playerInputs; //Script that includes inputs.
+	public EnemyState currentEnemyState; 
+
+	public enum EnemyState { //Character States
+        Idle, Move, Alert, //Traversal states (movement). 
+        Reload, Attack, Dodge //Action states (evade, attacks, etc).
 	}
 
+	public override void _Ready() {
+		playerCameraReference = GetNode<Camera2D>("/root/GameWorld/SidMarshall/PlayerCamera");
+		playerHealthBar = GetNode<ProgressBar>("/root/GameWorld/UIElements/PlayerUI/PlayerUIAnchor/Health");
+		playerReference = GetNode<PlayerController>("/root/GameWorld/SidMarshall");
+		maximumHealth = playerReference.maximumHealth;
+		playerHealth = playerReference.currentHealth;
+		currentEnemyState = EnemyState.Idle; 
+	}
+	
 	public override void _Process(double delta) {}
 }
