b4db84c37c36b38cc1a17c0834d174d835f8b640
Arya <85712580+AryaSahay193@users.noreply.github.com>
2025-09-21 21:28:31 -0400

    Update EventManager.cs
    Minor changes, will add a lot more signals in the future, once development starts to progress

--- Redot/Scripts/Game Managers/EventManager.cs
+++ Redot/Scripts/Game Managers/EventManager.cs
@@ -3,13 +3,9 @@ using System;
 
 //Class that handles events in game, such as collection of collectibles, hitting or defeating enemies, talking to NPCs, etc.
 public partial class EventManager : Node { //Manager that deals with events in the game, such as story-events or collectables.
-	[Signal] public delegate void JigsawCollectedEventHandler(int amount); //Signal that is sent when a Collectible is collected.
+	[Signal] public delegate void AchievementActivatedEventHandler(bool achievementFlag); //Sginal that is sent when an achievement triggers.
 	[Signal] public delegate void EncounterStartedEventHandler(bool inBattleMode); //Signal that is sent once Sid enters a Gang Encounter.
-	[Signal] public delegate void EncounterEndedEventHandler(bool inBattleMode); //Sginal that is sent when the Gang Encounter ends.
-
-	public override void _Ready() {
-	}
-
-	public override void _Process(double delta) {
-	}
+	[Signal] public delegate void JigsawCollectedEventHandler(int amount); //Signal that is sent when a Collectible is collected.
+	[Signal] public delegate void PlayerDamageEventHandler(float amount); //Signal that handles damage given by the player.
+	[Signal] public delegate void EnemyDamageEventHandler(float amount); //Signal that handles damage given by the enemy.
 }
