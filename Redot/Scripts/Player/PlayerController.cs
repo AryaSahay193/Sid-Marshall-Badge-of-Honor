bed5abcae4383fe778ea8028f9fed792428e189f
Arya <85712580+AryaSahay193@users.noreply.github.com>
2025-09-23 19:07:15 -0400

    Update PlayerController.cs
    Working wall-jump, and semi-working crouch. Jump still needs tweaking, so hopefully I can fix all movements very soon

--- Redot/Scripts/Player/PlayerController.cs
+++ Redot/Scripts/Player/PlayerController.cs
@@ -14,14 +14,15 @@ public partial class PlayerController : CharacterBody2D {
     public int maximumJumps = 2, numberOfJumps = 0;
     public PlayerState currentState = PlayerState.Idle;
     public Vector2 characterVelocity;
+    public RayCast2D wallDetection;
 
-    private bool isCrouching = false; //By default.
+    private bool isCrouching = false, isStanding = true; //By default.
     private float jumpVelocity = -121.90f, horizontalJumpVelocity = 100.70f, gravityValue = 186.03f, airVelocity = 87.45f;
     private float acceleration = 2.65f, airAcceleration = 2.915f, airFriction = 1.272f, friction = 3.180f;
-    private float wallPushback = 1060.0f, wallJumpHeight = 1325.0f, wallSlideSpeed = 116.6f;
-    private float coyoteCounter = 0.53f, coyoteTime, slideFriction = 36.570f;
-    private Shape2D crouchCollision, normalCollision; 
-    private RayCast2D groundDetection, wallDetection, ceilingDetection;
+    private float wallPushback = 171.72f, wallJumpHeight = -187.09f, wallSlideSpeed = 100.17f;
+    private float coyoteCounter = 0.53f, coyoteTime, slideFriction = 3.657f;
+    [Export] private Shape2D crouchCollision, normalCollision;
+    private RayCast2D groundDetection, ceilingDetection; 
     private CollisionShape2D playerCollision;
     private Timer wallJumpTimer, landTimer;
     private InputManager inputManager; 
@@ -29,8 +30,6 @@ public partial class PlayerController : CharacterBody2D {
     public override void _Ready() {
         inputManager = GetNode<InputManager>("/root/InputManager");
         playerCollision = GetNode<CollisionShape2D>("PlayerCollision");
-        crouchCollision = GD.Load<Shape2D>("/root/Scenes/Resources/sid_crouch_collision.res"); //Preload for crouch-collision shape.
-        normalCollision = GD.Load<Shape2D>("/root/Scenes/Resources/sid_normal_collision.res"); //Preload for normal-collision shape.
         groundDetection = GetNode<RayCast2D>("GroundDetection"); //Raycast used for the Landing state.
         wallDetection = GetNode<RayCast2D>("WallDetection"); //Raycast used for wall-jump direction.
         wallJumpTimer = GetNode<Timer>("Timers/WallJumpTimer");
@@ -43,7 +42,8 @@ public partial class PlayerController : CharacterBody2D {
     //Main method of the player, reads code for each frame the game runs.
     public override void _PhysicsProcess(double delta) {
         horizontalDirection = inputManager.horizontalButton(); //Axis type, assigns X-axis controls (Left and Right).
-        characterVelocity = Velocity;
+        characterVelocity = Velocity * ((float)delta * 60.0f);
+        float FPSstabalizer = (float)delta * 60.0f;
  
         //State Machine with Conditions.
         StateConditions((float)delta);
@@ -59,8 +59,10 @@ public partial class PlayerController : CharacterBody2D {
                 else characterMovement(walkingSpeed, acceleration, friction);
                 break;
             case PlayerState.Jump :
-                characterJump((float)delta);
-                break;
+                if(numberOfJumps < maximumJumps) {
+                    characterVelocity = new Vector2(characterVelocity.X, jumpVelocity) * FPSstabalizer;
+                    numberOfJumps += 1;
+                } break;
             case PlayerState.Fall :
                 float airMoveParameter = -1000.0f;
                 characterVelocity = new Vector2(characterVelocity.X, characterVelocity.Y += gravityValue * (float)delta);
@@ -68,30 +70,52 @@ public partial class PlayerController : CharacterBody2D {
                 if(!IsOnFloor() && groundDetection.IsColliding()) currentState = PlayerState.Land;
                 break;
             case PlayerState.Land :
-                characterVelocity.X = 0.0f;
+                if(IsOnFloor()) characterVelocity.X = 0.0f;
                 landTimer.Start();
                 break;
             case PlayerState.Crouch :
-                if(!inputManager.crouchButton()) currentState = PlayerState.Idle;
-                else characterCrouch();
+                if(horizontalDirection != 0.0f) {
+                    if(characterVelocity.X == maximumSpeed * horizontalDirection) characterFriction((slideFriction/2.5f)); //Sliding
+                    else characterFriction(slideFriction); //Crouching
+                } else characterFriction(slideFriction);
+                if(isCrouching == true) {
+                    playerCollision.Shape = crouchCollision;
+                    playerCollision.Position = new Vector2(0, 9);
+                } else characterStand(); 
                 break;
             case PlayerState.Walled : 
-                characterVelocity.Y = Mathf.MoveToward(characterVelocity.Y, wallSlideSpeed, acceleration);
-                if(wallDetection.IsColliding() && horizontalDirection == 0.0f || !wallDetection.IsColliding()) currentState = PlayerState.Fall;
-                //if(Input.IsActionPressed("player_jump")) currentState = PlayerState.Jump; 
+                characterVelocity.Y = Mathf.MoveToward(characterVelocity.Y, wallSlideSpeed, acceleration) * FPSstabalizer;
+                if(inputManager.jumpButton()) {
+                    characterVelocity = new Vector2(-(wallPushback * horizontalDirection), wallJumpHeight) * FPSstabalizer;
+                    numberOfJumps = 0;
+                } break;
+            case PlayerState.Punch :
+                characterVelocity = Vector2.Zero;
                 break;
-        } //GD.Print("Current State: " + currentState);
+            case PlayerState.Kick :
+                characterVelocity = Vector2.Zero;
+                break;
+        }
         Velocity = characterVelocity; //Setting the Velocity Vector2 equal to velocity.
         flipCharacter(); //Flips the player (Raycasts, animations will be flipped in Animation script).
         MoveAndSlide(); //A necessary method to make movement work in Redot engine.
+        //GD.Print("Current State: " + currentState);
+        GD.Print("Standing? : " + isStanding, ", Crouching? : " + isCrouching);
     }
  
     public void StateConditions(float delta) { //Method that sets conditions for each state.
-        if(inputManager.jumpButton() && (IsOnWallOnly() || IsOnFloorOnly()) && numberOfJumps < maximumJumps) currentState = PlayerState.Jump;
-        if(IsOnWallOnly() && wallDetection.IsColliding()) currentState = PlayerState.Walled;
+        if(IsOnWallOnly() && wallDetection.IsColliding() && horizontalDirection != 0.0f) currentState = PlayerState.Walled;
         else if(IsOnFloorOnly()) {    
             if(horizontalDirection == 0.0f && characterVelocity.X == 0.0f) currentState = PlayerState.Idle;
             else currentState = PlayerState.Move;
+            if(inputManager.punchButton() /*&& isInBattleMode*/) currentState = PlayerState.Punch;
+            else if(inputManager.kickButton() /*&& isInBattleMode*/) currentState = PlayerState.Kick;
+            if(inputManager.crouchButton() == false) {
+                isCrouching = false; isStanding = true;
+            } else {
+                isCrouching = true; isStanding = false;
+                currentState = PlayerState.Crouch;
+            }
             coyoteCounter = coyoteTime; //CoyoteTime = 1.5f.
             characterVelocity.Y = 0.0f; //Resets the gravity.
             numberOfJumps = 0; //Resets the counter.
@@ -113,41 +137,24 @@ public partial class PlayerController : CharacterBody2D {
             }
         }
     }
- 
+
+    private void characterFriction(float decrementValue) { 
+        if(IsOnFloor() && horizontalDirection == 0.0f) characterVelocity.X = Mathf.MoveToward(characterVelocity.X, 0.0f, decrementValue); //Player-friction code.
+    }
+
     //Both in-air and on the ground.
     private void characterMovement(float endingSpeed, float incrementValue, float decrementValue) {
         if(horizontalDirection != 0.0f) {
             if(IsOnFloorOnly() && inputManager.runButton()) {
                 characterVelocity.X = Mathf.MoveToward(characterVelocity.X, maximumSpeed * horizontalDirection, acceleration * 2.0f); //Run-movement code;
             } else characterVelocity.X = Mathf.MoveToward(characterVelocity.X, endingSpeed * horizontalDirection, incrementValue); //Walk-movement code.
-        } else characterVelocity.X = Mathf.MoveToward(characterVelocity.X, 0.0f, decrementValue); //Player-friction code.
-    }
-
-    private void characterJump(float delta) {
-        if(inputManager.jumpButton()) {
-            if(IsOnFloorOnly() || numberOfJumps < maximumJumps) {
-                characterVelocity = new Vector2(characterVelocity.X, jumpVelocity) * (float)delta;
-                numberOfJumps += 1;
-            } else if(IsOnWallOnly() && wallDetection.IsColliding()) {
-                characterVelocity = new Vector2(-(wallPushback * horizontalDirection), wallJumpHeight);
-                numberOfJumps = 0;
-            }
-        }
+        } else characterFriction(decrementValue); //Player-friction code.
     }
  
-    private void characterCrouch() {
-        if(inputManager.crouchButton()) {
-            playerCollision.Shape = crouchCollision;
-            isCrouching = true;
-            if(characterVelocity.X != 0.0f) {
-                if(characterVelocity.X == maximumSpeed || characterVelocity.X == -maximumSpeed) characterVelocity.X = Mathf.MoveToward(characterVelocity.X, 0.0f, -(slideFriction/2)); //Sliding    
-                else characterVelocity.X = Mathf.MoveToward(characterVelocity.X, 0.0f, -slideFriction); //Crouching
-            }
-        } else {
-            playerCollision.Shape = normalCollision;
-            currentState = PlayerState.Idle;
-            isCrouching = false;
-        }
+    private void characterStand() {
+        playerCollision.Shape = normalCollision;
+        playerCollision.Position = new Vector2(0, 1);
+        currentState = PlayerState.Idle;
     }
 
     //Signal method for when timer finishes when landing on the floor.
