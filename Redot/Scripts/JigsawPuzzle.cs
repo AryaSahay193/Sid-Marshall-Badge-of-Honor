7378c472ae8bc3143d2e984bacbaebe14e4092e3
Arya <85712580+AryaSahay193@users.noreply.github.com>
2025-12-13 21:07:00 -0500

    Update JigsawPuzzle.cs
    Code works perfectly fine, but rather than using Tweens (which is flooding errors in the debugger), I'll try moving it in code manually.

--- Redot/Scripts/JigsawPuzzle.cs
+++ Redot/Scripts/JigsawPuzzle.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class JigsawPuzzle : Node2D {
 	private int jigsawCounter = 0, jigsawValue = 1; //Value of a jigsaw-puzzle.
-	private float tweenDistance = 12.0f, tweenDuration = 0.75f;
+	private float tweenDistance = 12.0f, tweenDuration = 0.50f;
 	private Vector2 startingPosition, finalPosition, currentPosition;
 	private AnimatedSprite2D collectibleAnimations;
 	private AudioStreamPlayer pickupSound;
@@ -12,31 +12,31 @@ public partial class JigsawPuzzle : Node2D {
 
 	public override void _Ready() {
 		singletonReference = GetNode<GlobalData>("/root/GlobalData");
-		jigsawCounter = singletonReference.jigsawCounter;
 		collectibleAnimations = GetNode<AnimatedSprite2D>("Animation");
 		pickupSound = GetNode<AudioStreamPlayer>("PickupSound");
-		jigsawTween = CreateTween().SetLoops();
+		jigsawCounter = singletonReference.jigsawCounter;
+		jigsawTween = GetTree().CreateTween().SetLoops();
+		startingPosition = Position;
 	}
 
 	public override void _PhysicsProcess(double delta) {
-		startingPosition = Position;
 		finalPosition = new Vector2(startingPosition.X, startingPosition.Y - tweenDistance);
 		tweenMovement();
 	}
 
-	private void tweenMovement() { 
-		jigsawTween.TweenProperty(this, "position", finalPosition, tweenDuration).From(startingPosition); //Moves the animation up
-		jigsawTween.TweenProperty(this, "position", startingPosition, tweenDuration).From(finalPosition); //Moves the animation down.
-		jigsawTween.Finished += tweenRepeat;
+	private void tweenMovement() {
+		if(jigsawTween.IsValid()) {
+			jigsawTween.TweenProperty(this, "position", finalPosition, tweenDuration).From(startingPosition); //Moves the animation up
+			jigsawTween.TweenProperty(this, "position", startingPosition, tweenDuration).From(finalPosition); //Moves the animation down.
+			jigsawTween.TweenCallback(Callable.From(tweenMovement));
+		} else jigsawTween.Kill();
 	}
-
-	//Signal method to call the method to repeat the Tween.
-	private void tweenRepeat() => jigsawTween.TweenCallback(Callable.From(tweenMovement));
 	
 	//Signal method for when the puzzle piece is caught.
 	private void whenBody2DEntered(CharacterBody2D body) {
 		if(body is PlayerController) {
 			jigsawCounter += jigsawValue; //Adds one to the counter.
+			jigsawTween.Stop(); //Stops the Tween.
 			pickupSound.Play(); //Plays a sound when caught.
 			this.QueueFree(); //Deletes the collectible.
 		}
