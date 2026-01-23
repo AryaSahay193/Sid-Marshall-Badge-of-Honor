0b432731c6111bd420ac421867ac72f4a6a67262
Arya <85712580+AryaSahay193@users.noreply.github.com>
2026-01-22 22:04:20 -0400

    Update GlobalData.cs
    Most recent GlobalData Autoload script, now is attached to a node as a scene autoload rather than a C# script autoload.

--- Redot/Scripts/Game Managers/GlobalData.cs
+++ Redot/Scripts/Game Managers/GlobalData.cs
@@ -3,27 +3,47 @@ using System;
 
 //Script that lets other scripts access data globally, a singleton that will be autoloaded on Game start.
 public sealed partial class GlobalData : Node {
+	[Export] public CameraScript playerCamera ; //Only one instance of the player-camera.
+	[Export] public DoorScript doorScript; //Publicly accessible door class.
+	[Export] public PlayerController playerController; //Only one instance of the player controller.
+	[Export] public PlayerUI playerInfo; //Only one instance of player-health isntance.
+
 	public float playerHealth, maximumHealth = 100.0f; //Health will carry over through the entire game.
 	public int jigsawCounter = 0, documentCounter = 0; //Collectibles will carry over throughout the game.
-	public Camera2D playerCameraReference; //Only one player-camera instance.
-	public PlayerController playerReference; //Only one player instance.
-	public ProgressBar playerHealthBar; //One player-health isntance.
-	public InputManager playerInputs; //Script that includes inputs.
-	public EnemyState currentEnemyState; 
+	public bool insideBuilding = false, isInBattleMode = false; //By default.
+	
+	public EnemyState currentEnemyState;
+	public Vector2 playerVelocity;
 
 	public enum EnemyState { //Character States
-        Idle, Move, Alert, //Traversal states (movement). 
+        Idle, Move, BattleIdle, BattleMove, Alert, //Traversal states (movement). 
         Reload, Attack, Dodge //Action states (evade, attacks, etc).
 	}
 
 	public override void _Ready() {
-		playerCameraReference = GetNode<Camera2D>("/root/GameWorld/SidMarshall/PlayerCamera");
-		playerHealthBar = GetNode<ProgressBar>("/root/GameWorld/UIElements/PlayerUI/PlayerUIAnchor/Health");
-		playerReference = GetNode<PlayerController>("/root/GameWorld/SidMarshall");
-		maximumHealth = playerReference.maximumHealth;
-		playerHealth = playerReference.currentHealth;
+		insideBuilding = IsInGroup("Environment - Inside");
+		playerVelocity = playerController.characterVelocity;
+		maximumHealth = playerController.maximumHealth;
+		playerHealth = playerController.currentHealth;
 		currentEnemyState = EnemyState.Idle; 
+		playerController.IsOnFloor();
 	}
-	
-	public override void _Process(double delta) {}
+
+	public float randomDecimal() {
+        Random randomDecimal = new Random();
+		float decimalValue = (float)randomDecimal.NextDouble();
+		return decimalValue;
+    }
+
+	public int randomNumber(int startingRange, int endingRange) {
+        Random randomNumber = new Random();
+		int generatedNumber = randomNumber.Next(startingRange, endingRange);
+		return generatedNumber;
+    }
+
+	public int randomSign() {
+		Random numberSign = new Random();
+		int signValue = (int)Mathf.Pow(-1, numberSign.Next(0, 1));
+		return signValue;
+    }
 }
