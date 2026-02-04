347890fba9fe14350b30f0c5fb3395ca4e5d6fe8
Arya <85712580+AryaSahay193@users.noreply.github.com>
2026-02-03 20:42:46 -0400

    Update StateHandler.cs
    Latest state-handler for the State Machine

--- Redot/Scripts/Player/State Machine/StateHandler.cs
+++ Redot/Scripts/Player/State Machine/StateHandler.cs
@@ -3,30 +3,29 @@ using System;
 using System.Collections.Generic;
 
 public partial class StateHandler : Node {
+	public ParentState currentState; //Reference to the State class.
 	[Export] public NodePath startingState; //Starting path is Idle, in Redot Inspector.
-	private Dictionary<String, BaseStateClass> stateName; //Nodepath, as a string, which is stored in a Dictionary.
-	private BaseStateClass currentState; //Reference to the State class.
+	private Dictionary<String, ParentState> stateName; //Nodepath, as a string, which is stored in a Dictionary.
 
 	public override void _Ready() {
-		currentState = new BaseStateClass();
-		stateName = new Dictionary<String, BaseStateClass>();
+		currentState = new ParentState();
+		stateName = new Dictionary<String, ParentState>();
 		foreach(Node node in GetChildren()) {
-			if(node is BaseStateClass state) {
+			if(node is ParentState state) {
 				stateName[node.Name] = state;
 				state.finiteStateMachine = this;
-				state._Ready();
-				state.ExitState();
+				state._Ready(); //Initialize states.
+				state.ExitState(); //Reset the states.
 			}
 		}
-
-		currentState = GetNode<BaseStateClass>(startingState);
+		currentState = GetNode<ParentState>(startingState);
 		currentState.EnterState();
 	}
 
 	//Delegates the methods from BaseStateClass to methods commonly used.
 	public override void _Process(double delta) => currentState.UpdateState((float)delta); 
 	public override void _PhysicsProcess(double delta) => currentState.PhysicsUpdate((float)delta);
-	//public override void _UnhandledInput(InputEvent @event) => currentState.HandleInput(@event);
+	public override void _UnhandledInput(InputEvent @event) => currentState.HandleInput(@event);
 
 	public void StateTransition(string dictionaryKey) {
 		if(!stateName.ContainsKey(dictionaryKey) || currentState == stateName[dictionaryKey]) return; //If key is not found, or if key is already in the dictionary, do nothing.
