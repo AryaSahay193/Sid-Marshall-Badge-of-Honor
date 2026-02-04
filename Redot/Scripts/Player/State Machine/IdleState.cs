88ca7b76f1ef0ee1b447dbd21b2f47fa0c0449fd
Arya <85712580+AryaSahay193@users.noreply.github.com>
2026-02-03 20:39:54 -0400

    Update IdleState.cs
    Latest Idle state for the player State Machine

--- Redot/Scripts/Player/State Machine/IdleState.cs
+++ Redot/Scripts/Player/State Machine/IdleState.cs
@@ -2,20 +2,27 @@ using Godot;
 using System;
 using System.Runtime.CompilerServices;
 
-public partial class IdleState : BaseStateClass {
-	//Handles code when entering Idle-State.
-    public override void EnterState() { 
-		playerReference.characterVelocity = Vector2.Zero;
-		playerReference.playerAnimations.Play("Idle"); 
+public partial class IdleState : ParentState { //Handles code when entering Idle-State.
+    public override void EnterState() {
+		GD.Print("Idle State");
+		playerController.characterVelocity = Vector2.Zero;
+		playerAnimations.Play("Idle");
+	}
+	
+	public override void UpdateState(float delta) {
+		if(!playerController.IsOnFloor() || characterVelocity.Y > 0.0f) finiteStateMachine.StateTransition("FallState");
+		else playerAnimations.Play("Idle");
+		flipCharacter();
 	}
 
-	public override void UpdateState(float delta) {
-		if(playerReference.IsOnFloor() && playerReference.characterVelocity.X != 0.0f) {
-			finiteStateMachine.StateTransition("Sid_Move"); //Change to Move State.
-		} else if(!playerReference.IsOnFloor() && playerReference.characterVelocity.Y <= 0.0f) {
-			finiteStateMachine.StateTransition("Sid_Jump"); //Change to Jump State.
-		} else if(!playerReference.IsOnFloor() && playerReference.characterVelocity.Y >= 0.0f) {
-			finiteStateMachine.StateTransition("Sid_Fall"); //Chagne to Fall State.
-		}
+    public override void PhysicsUpdate(float delta) {
+		playerController.characterVelocity = Vector2.Zero;
+    }
+
+
+    public override void HandleInput(InputEvent @event) { //Changes states if they involve button presses.
+        if(inputManager.horizontalButton() != 0.0f) finiteStateMachine.StateTransition("MoveState");
+		else if(inputManager.jumpButton()) finiteStateMachine.StateTransition("JumpState");
+		else if(inputManager.lightAttackButton() || inputManager.heavyAttackButton()) finiteStateMachine.StateTransition("AttackCombo1");
 	}
 }
