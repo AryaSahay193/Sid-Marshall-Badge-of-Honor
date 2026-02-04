356372319af84e52460d7c3976c7f2b44caf117f
Arya <85712580+AryaSahay193@users.noreply.github.com>
2026-02-03 20:40:59 -0400

    Update JumpState.cs
    Latest Jump State for the player State Machine

--- Redot/Scripts/Player/State Machine/JumpState.cs
+++ Redot/Scripts/Player/State Machine/JumpState.cs
@@ -1,29 +1,36 @@
 using Godot;
 using System;
 
-public partial class JumpState : BaseStateClass {
+public partial class JumpState : ParentState {
 	[Export] private AudioStreamPlayer jumpSoundEffect;
 	private float wallPushback = 10.0f, wallJumpHeight = 13.0f, wallSlideSpeed;
-	private float jumpVelocity = -132.5f, pitchValue = 1.2f;
+	private float jumpVelocity = -119.25f, pitchValue = 1.2f;
 	private int maximumJumps = 2, numberOfJumps = 0;
 
 	//Handles code when entering the Jump State.
     public override void EnterState() {
-		playerReference.characterVelocity.Y = jumpVelocity;
-		playerReference.playerAnimations.Play("Jump");
-		jumpSoundEffect.Play();
-		numberOfJumps += 1;
+		GD.Print("Jump State");
+		numberOfJumps = maximumJumps;
+		characterVelocity = new Vector2(characterVelocity.X, jumpVelocity);
+		playerAnimations.Play("Jump");
 	}
 	
-	public override void UpdateState(float delta) { 
-		if(numberOfJumps == 1) { 
-			jumpSoundEffect.PitchScale *= pitchValue;
-			playerReference.playerAnimations.Play("Double_Jump"); 
-		} if(numberOfJumps == 0) numberOfJumps += 1;
+	public override void UpdateState(float delta) {
+		if(inputManager.jumpButton()) {
+			jumpSoundEffect.Play();
+			numberOfJumps += 1;
+		}
 
-		if(playerReference.IsOnFloor()) finiteStateMachine.StateTransition(IdleState); 
-		else if(!playerReference.IsOnFloor() && playerReference.characterVelocity.Y >= 0.0f) finiteStateMachine.StateTransition(FallState);
-		//else if(Input.IsActionJustPressed("Kick")) finiteStateMachine.StateTransition("Sid_Kick");
-		//else if(Input.IsActionJustPressed("Punch")) finiteStateMachine.StateTransition("Sid_Punch");
+		if(playerController.IsOnFloor()) finiteStateMachine.StateTransition("IdleState"); 
+		else if(!playerController.IsOnFloor() && characterVelocity.Y >= 0.0f) finiteStateMachine.StateTransition("FallState");
+		
+		if(numberOfJumps == 2) { 
+			jumpSoundEffect.PitchScale *= pitchValue;
+			playerAnimations.Play("Double_Jump"); 
+		} if(numberOfJumps == 0) numberOfJumps += 1;
 	}
+
+    public override void PhysicsUpdate(float delta) {
+        characterVelocity = new Vector2(characterVelocity.X, jumpVelocity);
+    }
 }
