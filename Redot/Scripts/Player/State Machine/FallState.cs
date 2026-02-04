c3f051b7b2f83579b46458e7cfc5015d1c49d2dd
Arya <85712580+AryaSahay193@users.noreply.github.com>
2026-02-03 20:41:31 -0400

    Update FallState.cs
    Latest Fall State for the player State Machine

--- Redot/Scripts/Player/State Machine/FallState.cs
+++ Redot/Scripts/Player/State Machine/FallState.cs
@@ -1,24 +1,34 @@
 using Godot;
 using System;
 
-public partial class FallState : BaseStateClass {
-	private float airVelocity = 26.5f, airAcceleration = 4.24f, coyoteCounter = 1.5f;
+public partial class FallState : ParentState {
+	private float airAcceleration = 2.915f, coyoteCounter = 1.06f;
 	private Vector2 fallMovement;
 
 	//Handles code when entering the Fall State.
-    public override void EnterState() => playerReference.playerAnimations.Play("Fall");
+    public override void EnterState() { 
+		GD.Print("Fall State");
+		playerAnimations.Play("Fall");
+	}
 	
 	public override void UpdateState(float delta) {
-		if(playerReference.IsOnFloor() || playerReference.IsOnWall()) coyoteCounter = 0.0f;
-		else coyoteCounter -= delta;
-
-		if(playerReference.IsOnFloor()) finiteStateMachine.StateTransition("Sid_Idle");
+		if(playerController.IsOnFloor()) finiteStateMachine.StateTransition("IdleState");
+		flipCharacter();
 		//if(characterHealth == 0) finiteState.Machine.StateTransition("Sid_Death");
+		//if(playerReference.IsOnFloor() || playerReference.IsOnWall()) coyoteCounter = 0.0f;
+		//else coyoteCounter -= delta;
 	}
 
 	//Handles code that deals with physics-related movement.
 	public override void PhysicsUpdate(float delta) {
-		float fallMovementX = Mathf.MoveToward(playerReference.characterVelocity.X, airVelocity, airAcceleration) * playerReference.moveDirection.X;
-		fallMovement += new Vector2(fallMovementX, playerReference.gravityValue * delta);
+		//characterVelocity.Y += (float)Math.Pow(playerController.gravityValue, 1.1f) * (float)delta; //Sets the character gravity.
+		//GD.Print("Fall Velocity: " + characterVelocity);
+		//float airMoveParameter = -1000.0f;
+		//characterVelocity.Y += playerController.gravityValue;
+		//characterVelocity = new Vector2(characterVelocity.X, characterVelocity.Y += playerController.gravityValue * (float)delta);
+		/*if(characterVelocity.Y >= airMoveParameter) {
+			if(airborneTimer.WaitTime - airborneTimer.TimeLeft <= 2.0f && groundDetection.IsColliding() && !IsOnWallOnly()) currentState = PlayerState.Land;
+			else characterMovement(airVelocity, airAcceleration, airFriction);
+		}*/
 	}
 }
