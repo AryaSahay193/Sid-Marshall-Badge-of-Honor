a0189c15ec8ec1fa8ff1d91f17313028db18f2e1
Arya <85712580+AryaSahay193@users.noreply.github.com>
2026-02-04 22:02:05 -0400

    Update IdleState.cs
    Working idle-state

--- Redot/Scripts/Player/State Machine/IdleState.cs
+++ Redot/Scripts/Player/State Machine/IdleState.cs
@@ -4,25 +4,26 @@ using System.Runtime.CompilerServices;
 
 public partial class IdleState : ParentState { //Handles code when entering Idle-State.
     public override void EnterState() {
-		GD.Print("Idle State");
-		playerController.characterVelocity = Vector2.Zero;
 		playerAnimations.Play("Idle");
 	}
 	
 	public override void UpdateState(float delta) {
-		if(!playerController.IsOnFloor() || characterVelocity.Y > 0.0f) finiteStateMachine.StateTransition("FallState");
-		else playerAnimations.Play("Idle");
+		playerAnimations.Play("Idle");
+		if(playerController.Velocity.Y > 0.0f) finiteStateMachine.StateTransition("FallState");
 		flipCharacter();
 	}
 
     public override void PhysicsUpdate(float delta) {
-		playerController.characterVelocity = Vector2.Zero;
+		playerController.Velocity = playerController.Velocity with {X = 0.0f};
     }
 
 
     public override void HandleInput(InputEvent @event) { //Changes states if they involve button presses.
-        if(inputManager.horizontalButton() != 0.0f) finiteStateMachine.StateTransition("MoveState");
-		else if(inputManager.jumpButton()) finiteStateMachine.StateTransition("JumpState");
-		else if(inputManager.lightAttackButton() || inputManager.heavyAttackButton()) finiteStateMachine.StateTransition("AttackCombo1");
+		if(playerController.IsOnFloorOnly()) {
+			if(inputManager.lightAttackButton() || inputManager.heavyAttackButton()) finiteStateMachine.StateTransition("AttackCombo1");
+			else if(inputManager.horizontalButton() != 0.0f) finiteStateMachine.StateTransition("MoveState");
+			else if(inputManager.crouchButton()) finiteStateMachine.StateTransition("CrouchState");
+			else if(inputManager.jumpButton()) finiteStateMachine.StateTransition("JumpState");
+		}
 	}
 }
