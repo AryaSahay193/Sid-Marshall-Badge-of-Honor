3cf8bf5b071178ae01ecc37147f2d66c1d23d6d9
Arya <85712580+AryaSahay193@users.noreply.github.com>
2026-02-04 22:01:25 -0400

    Update FallState.cs
    Working fall-state

--- Redot/Scripts/Player/State Machine/FallState.cs
+++ Redot/Scripts/Player/State Machine/FallState.cs
@@ -2,33 +2,24 @@ using Godot;
 using System;
 
 public partial class FallState : ParentState {
-	private float airAcceleration = 2.915f, coyoteCounter = 1.06f;
-	private Vector2 fallMovement;
+	private float airAcceleration = 2.915f, airFriction = 12.72f;
+	private float airMovement = 172.25f, coyoteCounter = 1.06f;
 
 	//Handles code when entering the Fall State.
     public override void EnterState() { 
-		GD.Print("Fall State");
 		playerAnimations.Play("Fall");
+		GD.Print("Fall State");
 	}
 	
 	public override void UpdateState(float delta) {
-		if(playerController.IsOnFloor()) finiteStateMachine.StateTransition("IdleState");
+		if(playerController.IsOnFloorOnly()) finiteStateMachine.StateTransition("IdleState");
 		flipCharacter();
-		//if(characterHealth == 0) finiteState.Machine.StateTransition("Sid_Death");
-		//if(playerReference.IsOnFloor() || playerReference.IsOnWall()) coyoteCounter = 0.0f;
-		//else coyoteCounter -= delta;
 	}
 
 	//Handles code that deals with physics-related movement.
 	public override void PhysicsUpdate(float delta) {
-		//characterVelocity.Y += (float)Math.Pow(playerController.gravityValue, 1.1f) * (float)delta; //Sets the character gravity.
-		//GD.Print("Fall Velocity: " + characterVelocity);
-		//float airMoveParameter = -1000.0f;
-		//characterVelocity.Y += playerController.gravityValue;
-		//characterVelocity = new Vector2(characterVelocity.X, characterVelocity.Y += playerController.gravityValue * (float)delta);
-		/*if(characterVelocity.Y >= airMoveParameter) {
-			if(airborneTimer.WaitTime - airborneTimer.TimeLeft <= 2.0f && groundDetection.IsColliding() && !IsOnWallOnly()) currentState = PlayerState.Land;
-			else characterMovement(airVelocity, airAcceleration, airFriction);
-		}*/
+		if(inputManager.horizontalButton() != 0.0) {
+			playerController.Velocity = playerController.Velocity with {X = Mathf.MoveToward(playerController.Velocity.X, airMovement * inputManager.horizontalButton(), airAcceleration)};
+		} else playerController.Velocity = playerController.Velocity with {X = Mathf.MoveToward(playerController.Velocity.X, 0.0f, airFriction)};
 	}
 }
