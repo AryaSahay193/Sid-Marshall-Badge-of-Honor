40c02b373c4207273c442d7d2fa71a6bec853af3
Arya <85712580+AryaSahay193@users.noreply.github.com>
2026-02-06 21:18:03 -0400

    Update WallJumpState.cs
    Working wall-jump state, in the future I might fix some minor bugs or add new features if needed

--- Redot/Scripts/Player/State Machine/WallJumpState.cs
+++ Redot/Scripts/Player/State Machine/WallJumpState.cs
@@ -1,13 +1,26 @@
 using Godot;
 using System;
+using System.Runtime.CompilerServices;
 
-public partial class WallJumpState : BaseStateClass {
-	public override void EnterState() {
+public partial class WallJumpState : ParentState { //Handles code when entering Idle-State.
+    private float wallPushback = 343.44f, wallJumpHeight = -227.90f, wallSlideSpeed = 1298.50f, wallSlideAcceleration = 26.50f;
+    
+    public override void EnterState() {
+		playerController.debugText.Text = "[center]State: Wall[/center]";
+		playerAnimations.Play("Wall_Contact");
 	}
-
+	
 	public override void UpdateState(float delta) {
+		if(playerController.IsOnWall() && inputManager.horizontalButton() != 0.0f) {
+			if(inputManager.jumpButton()) playerAnimations.Play("Wall_Kick");
+			else playerAnimations.Play("Wall_Slide");
+		} else finiteStateMachine.StateTransition("FallState");
+		flipCharacter();
 	}
 
-	public override void ExitState() {
-	}
+    public override void PhysicsUpdate(float delta) {
+		playerController.Velocity = playerController.Velocity with {Y = 0.0f}; //Cancels gravity when holding towards a wall.
+		if(inputManager.jumpButton()) playerController.Velocity = new Vector2((wallPushback * -inputManager.horizontalButton()), wallJumpHeight);
+		else playerController.Velocity = new Vector2(playerController.Velocity.X, Mathf.MoveToward(playerController.Velocity.Y, wallSlideSpeed, wallSlideAcceleration));
+    }
 }
