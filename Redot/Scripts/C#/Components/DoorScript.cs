6621f29be44dffb78b185a53be42753258d24fe6
Arya <85712580+AryaSahay193@users.noreply.github.com>
2026-02-28 12:37:39 -0400

    Reorganization

---
+++ Redot/Scripts/C#/Components/DoorScript.cs
@@ -0,0 +1,65 @@
+using Godot;
+using System;
+
+public partial class DoorScript : Node2D {
+	public bool areaDetected = false, doorOpened = false;
+	public event Action sidOpenDoor;
+
+	private PlayerEffects playerEffects;
+	private GlobalData singletonReference;
+	private InputManager inputManager;
+	private SceneManager sceneManager;
+
+	[ExportGroup("Animations")]
+	[Export] private AnimatedSprite2D doorAnimations;
+	
+	[ExportGroup("Collisions")]
+	[Export] private Area2D playerDetection;
+
+	[ExportGroup("Sound Effects")]
+	[Export] private AudioStreamPlayer doorOpenSFX, doorCloseSFX;
+
+	[ExportGroup("References")]
+	[Export] public Marker2D spawnLocation;
+	[Export] private PackedScene playerInstance;
+	[Export(PropertyHint.File, "*.tscn")] private String scenePath; //Accepts any file type of .tscn
+
+	public override void _Ready() {
+        singletonReference = GetNode<GlobalData>("/root/GlobalData");
+		inputManager = GetNode<InputManager>("/root/InputManager");
+		sceneManager = GetNode<SceneManager>("/root/SceneManager");
+		playerEffects = singletonReference.playerEffects;
+		playerDetection.BodyEntered += rangeEntered;
+		playerDetection.BodyExited += rangeExited;
+
+		if(singletonReference.insideBuilding) doorAnimations.Play("Default_Inside");
+		else doorAnimations.Play("Default_Outside");
+    }
+
+	public override void _Process(double delta) {
+		if(areaDetected && !doorOpened) { //DoorOpened - Flag boolean which will only make the door open once. 
+			if(inputManager.verticalButton() < 0.0f) {
+				doorOpenSFX.Play();
+				doorAnimations.Play("Enter_Inside");
+				sidOpenDoor?.Invoke();
+				doorAnimations.AnimationFinished += () => sceneManager.transitionToArea(scenePath);
+			} else return;
+			doorOpened = true; //Sets door open after opening. 
+		} 
+    }
+
+	public void rangeEntered(Node2D sidMarshall) {
+		if(sidMarshall is PlayerController) areaDetected = true;
+		else return;
+	}
+
+	public void rangeExited(Node2D sidMarshall) {
+		if(sidMarshall is PlayerController) areaDetected = false;
+	}
+
+	/*private void instantiatePlayer() {
+		playerInstance.Instantiate(); //Creates a new instance of Sid Marshall.
+		GetTree().CurrentScene.AddChild(playerReference); //Adds Sid as a child to the current scene.
+		playerReference.Position = spawnLocation.Position;
+	}*/
+}
