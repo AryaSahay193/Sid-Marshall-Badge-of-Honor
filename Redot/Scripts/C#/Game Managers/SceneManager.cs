4dc4c84541a350784c38b38c713485f5c87a7180
Arya <85712580+AryaSahay193@users.noreply.github.com>
2026-03-31 21:19:51 -0400

    Update SceneManager.cs
    Most recent Scene Manager script, it still has a weird bug that instantiates all scenes from doors that have scene paths attached to them, but hopefully I can figure this out in the future where only one scene generates at a time.

--- Redot/Scripts/C#/Game Managers/SceneManager.cs
+++ Redot/Scripts/C#/Game Managers/SceneManager.cs
@@ -1,68 +1,62 @@
 using Godot;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 public sealed partial class SceneManager : CanvasLayer {
 	public event Action sceneChanged; //Signal emitted when the scene finishes loading.
 
 	[ExportGroup("Properties")]
-	[Export] private float transitionDuration;
-	[Export] private AnimationPlayer screenFade;
+	[Export] private float fadeDuration;
 	[Export] private ColorRect screenTransition;
 
 	[ExportGroup("References")]
-	[Export(PropertyHint.File, "*.tscn")] private string playerPath, doorPath; //Accepts any .tscn file.
+	[Export(PropertyHint.File, "*.tscn")] private string doorPath; //Accepts any .tscn file.
 	
-	private DoorScript doorScript;
-	private Node sceneLocation, currentScene, previousScene;
-	private PlayerController sidMarshall;
+	private PlayerController playerController;
 	private GlobalData singletonReference;
+	private DoorScript doorScript;
+	private Node nextScene, previousScene;
+	private Node2D sceneStorage;
 	private Tween fadeTransition;
-	private String scenePath;
+	private String nodePath;
 
 	public override void _Ready() {
 		singletonReference = GetNode<GlobalData>("/root/GlobalData");
-		sceneLocation = GetNode<Node2D>("/root/GameWorld/Areas/"); //Path to stored currentScene
-		sidMarshall = singletonReference.playerController;
+		sceneStorage = singletonReference.sceneStorage; //Path to stored currentScene
+		playerController = singletonReference.playerController;
 		doorScript = singletonReference.doorScript;
-		scenePath = doorScript.sceneLocation;
+		nodePath = doorScript.sceneLocation;
 	}
 
-    public override void _Process(double delta) {
-        doorScript.changeToScene += (scenePath) => transitionToArea(scenePath);
-    }
-
-
-	public void transitionToArea(String scenePath) {
-		screenFade.Play("Fade");
-		screenTransition.Visible = true;
-		previousScene = sceneLocation.GetChild(1); //Stores current scene.
-		if(sceneLocation != null) {
-			var nextScene = ResourceLoader.Load<Node2D>(scenePath); //Instantiating scene as Node2D
-			if(sceneLocation.GetChildCount() >= 1) { //Delete every single area under Main world Areas path to add new scene.
-				for(int childAmount = 0; childAmount <= sceneLocation.GetChildCount(); childAmount++) {
-					if(sceneLocation.GetChildCount() != 0.0f) sceneLocation.GetChild(childAmount).QueueFree(); //Deletes all scenes if more than one.
-					else sceneLocation.AddChild(nextScene);
-				}
-			} 
-			instantiatePlayer(playerPath);
-			currentScene = nextScene;
-		} else instantiatePlayer(playerPath);
-		GetTree().ChangeSceneToFile(scenePath); //Changes scene.
-		screenTransition.Visible = false;
-		screenFade.PlayBackwards("Fade");
+	public void sceneTransition(String sceneDirectory) {
+		//Tween screenFade = GetTree().CreateTween();
+		//screenFade.TweenProperty(screenTransition, "modulate", new Color("#000000"), fadeDuration); //Fades to black.
+		//screenTransition.Visible = true;
+		sceneStorage.GetChild(0).QueueFree();
+		instantiateScene(sceneDirectory); //Adds the new scene as a child.
+		spawnPlayerAtDoor();
+		sceneChanged?.Invoke(); //Sends signal to finish Door State. 
+		//screenFade.TweenProperty(screenTransition, "modulate", new Color("#00000000"), fadeDuration); //Fades to alpha/transparent.
 	}
 	
-	public void transitionToScreen(PackedScene nextScreen) {
+	/*public void menuTransition(PackedScene nextScreen) {
 		fadeTransition = GetTree().CreateTween();
-		//fadeTransition.SetPauseMode(Tween.TweenProcessMode.Process);
-		fadeTransition.TweenProperty(screenTransition, "modulate", new Color("#000000"), transitionDuration); //Fades to black.
+		fadeTransition.TweenProperty(screenTransition, "modulate", new Color("#000000"), fadeDuration); //Fades to black.
 		GetTree().ChangeSceneToPacked(nextScreen);
-		fadeTransition.TweenProperty(screenTransition, "modulate", new Color("#00000000"), transitionDuration); //Fades to alpha.
-    }
+		fadeTransition.TweenProperty(screenTransition, "modulate", new Color("#00000000"), fadeDuration); //Fades to alpha/transparent.
+    }*/
 
-	private void instantiatePlayer(String pathToPlayer) {
-		var playerScene = ResourceLoader.Load<PackedScene>(pathToPlayer); //Loads the player.
-		sidMarshall = playerScene.Instantiate<PlayerController>(); //Creates a new instance of the player.
-		sceneLocation.GetChild(1).AddChild(sidMarshall); //Adds the player as a child to the new scene.
+	private void instantiateScene(String pathToScene) {
+		Node2D sceneBlueprint = GD.Load<PackedScene>(pathToScene).Instantiate<Node2D>();
+		sceneBlueprint.GlobalPosition = playerController.GlobalPosition; //Sets the scene at the player's location.
+		sceneStorage.AddChild(sceneBlueprint); //Adds the newly instantiated object as a child of the scene tree.
+		nextScene = sceneBlueprint;
+	}
+
+	private async void spawnPlayerAtDoor() {
+		nextScene.FindChild(doorPath);
+		if(nextScene.HasNode(doorPath)) playerController.GlobalPosition = doorScript.spawnLocation.GlobalPosition;
+		else GD.Print("This scene does not contain a door.");
 	}
 }
