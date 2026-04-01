c5d4594c53774a3b93907059ed5892b438f884c0
Arya <85712580+AryaSahay193@users.noreply.github.com>
2026-03-31 21:18:17 -0400

    Update DoorState.cs
    Latest Door State, which will be responsible for keeping the player in the same state until the scene finishes changing. For now the beginning part works only but in the future I'll try to make it fully functional.

--- Redot/Scripts/C#/Player/State Machine/DoorState.cs
+++ Redot/Scripts/C#/Player/State Machine/DoorState.cs
@@ -2,8 +2,7 @@ using Godot;
 using System;
 
 public partial class DoorState : ParentState {
-	public event Action doorUnlocked, enteredScene;
-	public DoorState doorStateReference;
+	public event Action sceneChange;
 	private bool doorOpened = false;
 
     public override void _EnterTree() {
@@ -14,27 +13,30 @@ public partial class DoorState : ParentState {
 	public override void EnterState() {
 		playerController.debugText.Text = "[center]State: Door[/center]";
 		playerAnimations.Play("Door_Open");
-		playerAnimations.AnimationFinished += () => doorUnlocked?.Invoke();
-		doorUnlocked += () => EnterDoorAnimation();
+		playerAnimations.AnimationFinished += () => EnterDoorAnimation();
+		doorScript.insideHouse += ExitState; //Executes only once.
 	}
 	
 	public override void PhysicsUpdate(float delta) {
-		playerController.Velocity = playerController.Velocity with {X = 0.0f};
+		playerController.Velocity = Vector2.Zero;
 		SetPhysicsProcess(false);
 		SetProcessInput(false);
 	}
 
 	public override void ExitState() {
-		//playerAnimations.Play("Door_Exit_Pull");
+		playerAnimations.Play("Door_Exit_Pull");
+		playerAnimations.AnimationFinished += () => SetPhysicsProcess(true);
+		finiteStateMachine.ChangeStateTo("IdleState");
 	}
 
 	private void EnterDoorAnimation() {
 		if(!doorOpened) {
 			playerAnimations.Play("Door_Enter_Push");
+			doorScript.openDoor(); //Executes only once.
 			if(playerAnimations.IsPlaying()) {
 				Tween colorChange = GetTree().CreateTween();
 				colorChange.TweenProperty(playerAnimations, "modulate", new Color("#2d1e2f"), animationLengthOf("Door_Enter_Push"));
-			} playerAnimations.AnimationFinished += () => enteredScene?.Invoke();
+			} playerAnimations.AnimationFinished += () => sceneChange?.Invoke(); //Signal emitted to initiate scene change.
 		} doorOpened = true; 
 	}
 }
