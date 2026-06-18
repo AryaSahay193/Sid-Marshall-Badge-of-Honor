17157e4d73c704770f1f5ae23315e33303e334c3
Arya <85712580+AryaSahay193@users.noreply.github.com>
2026-06-17 21:20:39 -0400

    Update PlayerWalled.gd
    Working wall-jump state

--- Redot/Scripts/GDScript/Player/State Machine/State Machine/PlayerWalled.gd
+++ Redot/Scripts/GDScript/Player/State Machine/State Machine/PlayerWalled.gd
@@ -6,17 +6,17 @@ var wallPushback : float = 343.44; var wallJumpHeight : float = -227.9;
 var wallSlideSpeed : float = 1298.50; var wallSlideAcceleration : float = 26.5;
 
 func EnterState() :
-	playerController.debugText.text = "[center]State: Wall[/center]"
-	playerController.playerAnimations.play("Wall_Contact")
+	playerAnimations.play("Wall_Contact")
+	# print("State: Walled")
 
 func UpdateState(_delta : float) :
-	if (playerController.is_on_wall() && playerController.wallDetection.is_colliding() && inputManager.horizontalButton() != 0.0) :
-		if inputManager.jumpButton() : playerController.playerAnimations.play("Wall_Kick")
-		else : playerController.playerAnimations.play("Wall_Slide")
-	else : finiteStateMachine.StateTransition("FallState")
-	playerController.flipCharacter()
+	if (playerReference.is_on_wall() && playerReference.wallDetection.is_colliding() && InputManager.horizontalButton() != 0.0) :
+		if InputManager.jumpButton() : playerAnimations.play("Wall_Kick")
+		else : playerAnimations.play("Wall_Slide")
+	else : finiteStateMachine.changeToState("Fall")
+	flipCharacter()
 
 func PhysicsUpdate(_delta : float) :
-	playerController.velocity = Vector2(playerController.velocity.x, 0.0) # Cancels gravity when holding towards a wall.
-	if inputManager.jumpButton() : playerController.velocity = Vector2((wallPushback * -inputManager.horizontalButton()), wallJumpHeight)
-	else : playerController.velocity = Vector2(playerController.velocity.x, move_toward(playerController.velocity.y, wallSlideSpeed, wallSlideAcceleration))
+	playerReference.velocity = Vector2(playerReference.velocity.x, 0.0) # Cancels gravity when holding towards a wall.
+	if InputManager.jumpButton() : playerReference.velocity = Vector2((wallPushback * -InputManager.horizontalButton()), wallJumpHeight)
+	else : playerReference.velocity = Vector2(playerReference.velocity.x, move_toward(playerReference.velocity.y, wallSlideSpeed, wallSlideAcceleration))
