964fda2eb42a1ed8b6bb740b9b0ac7bc2f32396b
Arya <85712580+AryaSahay193@users.noreply.github.com>
2026-06-17 21:17:37 -0400

    Update PlayerMove.gd
    Working move state script, animations might be a bit off but for the most part everything is working!

--- Redot/Scripts/GDScript/Player/State Machine/State Machine/PlayerMove.gd
+++ Redot/Scripts/GDScript/Player/State Machine/State Machine/PlayerMove.gd
@@ -1,37 +1,35 @@
 # Handles code when entering Move-State.
-class_name PlayerMove
+class_name PlayerMove # Handles code when entering Move-State.
 extends StateParent
 
+@onready var isSkidding : bool = InputManager.horizontalButton() == 0 && playerReference.velocity.x != 0
 var walkingSpeed : float = 53.0; var maximumSpeed : float = 164.3; var acceleration : float = 2.65; var friction : float = 3.18 # Float types
 var grassWalkSFX : AudioStreamPlayer
 
-func EnterState() : # Enters the Move-State, code for walking animation and movement.
-	playerController.debugText.Text = "[center]State: Move[/center]"
-	if inputManager.horizontalButton() != 0.0 :
-		if inputManager.runButton() : playerController.playerAnimations.play("Run")
-		else : playerController.playerAnimations.play("Walk")
-
 func UpdateState(_delta : float) :
-	if playerController.is_on_floor() :
-		if playerController.velocity.x != 0.0 :
-			if inputManager.crouchButton() :
-				if playerController.velocity.x == maximumSpeed * inputManager.horizontalButton() : finiteStateMachine.StateTransition("SlideState") # Change to Slide State.
-				else : finiteStateMachine.StateTransition("CrouchState")
-			if inputManager.horizontalButton() != sign(playerController.velocity.x) : playerController.playerAnimations.play("Skid")
-			else : if inputManager.runButton() && (playerController.velocity.x > walkingSpeed || playerController.velocity.x < -walkingSpeed) : playerController.playerAnimations.Play("Run")
-			else : if playerController.velocity.x < walkingSpeed || playerController.velocity.x > -walkingSpeed : playerController.playerAnimations.play("Walk")
-		else : finiteStateMachine.StateTransition("IdleState") # Change to Idle State.
-	else : if playerController.velocity.y > 0.0 : finiteStateMachine.StateTransition("FallState") # Change to Fall State.
-	playerController.flipCharacter();
+	if InputManager.runButton() : playerReference.playerAnimations.play("Run")
+	else : playerReference.playerAnimations.play("Walk")
+	
+	# if InputManager.horizontalButton() != sign(playerReference.velocity.x) : playerReference.playerAnimations.play("Skid")
+	#if InputManager.crouchButton() : 
+	#	if playerReference.velocity.x == maximumSpeed * InputManager.horizontalButton() : finiteStateMachine.changeToState("Slide") # Change to Slide State.
+	#	else : finiteStateMachine.changeToState("Crouch")
+
+	if InputManager.horizontalButton() != sign(playerReference.velocity.x) : playerReference.playerAnimations.play("Skid")
+	elif InputManager.horizontalButton() == 0 : 
+		if playerReference.velocity.x == 0.0 : finiteStateMachine.changeToState("Idle") # Change to Idle State.
+		else : playerReference.playerAnimations.play("Skid")
+	elif playerReference.velocity.y > 0.0 : finiteStateMachine.changeToState("Fall") # Change to Fall State.
+	flipCharacter()
 
 func PhysicsUpdate(_delta : float) : # Handles code that deals with physics-related movement.
-	if playerController.is_on_floor_only() :
-		if inputManager.horizontalButton() != 0.0 :
-			if inputManager.runButton() : playerController.velocity = Vector2(playerController.calculateVelocity(playerController.velocity.x, maximumSpeed, acceleration), playerController.velocity.y) # Run-movement code;
-			else : playerController.velocity = Vector2(playerController.calculateVelocity(playerController.velocity.x, walkingSpeed, acceleration), playerController.velocity.y) # Walk-movement code.
-		else : if inputManager.horizontalButton() == 0.0 && playerController.velocity.x != 0.0 : playerController.velocity = Vector2(playerController.calculateVelocity(playerController.velocity.x, 0.0, friction), playerController.velocity.y) # Player-friction code.
+	if InputManager.runButton() : playerReference.velocity = Vector2(calculateVelocity(playerReference.velocity.x, maximumSpeed, acceleration), playerReference.velocity.y) # Run-movement code;
+	else : playerReference.velocity = Vector2(calculateVelocity(playerReference.velocity.x, walkingSpeed, acceleration), playerReference.velocity.y) # Walk-movement code.
+
+func ExitState() :
+	playerReference.velocity = Vector2(calculateVelocity(playerReference.velocity.x, 0.0, friction), playerReference.velocity.y) # Player-friction code.
 
 func HandleInput(_event : InputEvent) :
-	if playerController.is_on_floor_only() && !playerController.is_on_wall() :
-		if inputManager.horizontalButton() == 0.0 && playerController.velocity.x == 0.0 : finiteStateMachine.StateTransition("IdleState") # Change to Idle State.
-		else : if inputManager.jumpButton() : finiteStateMachine.StateTransition("JumpState"); # Change to Jump State.
+	if playerReference.is_on_floor_only() && !playerReference.is_on_wall() :
+		if InputManager.horizontalButton() == 0 && playerReference.velocity.x == 0.0 : finiteStateMachine.changeToState("Idle") # Change to Idle State.
+		elif InputManager.jumpButton() : finiteStateMachine.changeToState("Jump") # Change to Jump State.
