d77d388cd117853570368cd7eddfed6f5e66533e
Arya <85712580+AryaSahay193@users.noreply.github.com>
2026-06-17 21:19:19 -0400

    Update PlayerJump.gd
    Most recent functioning jump state, double jump doesn't work but is a work in progress

--- Redot/Scripts/GDScript/Player/State Machine/State Machine/PlayerJump.gd
+++ Redot/Scripts/GDScript/Player/State Machine/State Machine/PlayerJump.gd
@@ -3,27 +3,28 @@ class_name PlayerJump
 extends StateParent
 
 @export var jumpSFX : AudioStreamPlayer
-var horizontalJumpVelocity : float = 100.7; var airVelocity : float = 87.45;
-var jumpVelocity : float = -182.85; var pitchValue : float = 1.2;
-var maximumJumps : int = 2; var numberOfJumps : int = 0;
+var horizontalJumpVelocity : float = 100.7; var airVelocity : float = 87.45
+var jumpVelocity : float = -182.85; var pitchValue : float = 1.2
+var coyoteCounter : float = 1.06; var coyoteTimer : float
+var maximumJumps : int = 2; var numberOfJumps : int = 0
 
 func EnterState() :
-	playerController.debugText.text = "[center]State: Jump[/center]";
-	playerController.velocity = Vector2(playerController.velocity.x, jumpVelocity)
-	playerController.playerAnimations.play("Jump")
+	playerReference.velocity = Vector2(playerReference.velocity.x, jumpVelocity)
+	playerAnimations.play("Jump")
+	# print("State: Jump")
 	jumpSFX.play()
 	numberOfJumps += 1
 
 func UpdateState(_delta : float) :
-	if !playerController.is_on_floor() :
-		if inputManager.jumpButton() && numberOfJumps < maximumJumps :
-			if numberOfJumps == 2 : playerController.playerAnimations.play("Double_Jump")
+	if !playerReference.is_on_floor() :
+		if InputManager.jumpButton() && numberOfJumps < maximumJumps :
+			if numberOfJumps == 2 : playerReference.playerAnimations.play("Double_Jump")
 			jumpSFX.PitchScale *= 1.2
 	else : numberOfJumps = 0
-	if (playerController.is_on_wall_only() && playerController.wallDetection.is_colliding() && inputManager.horizontalButton() != 0.0) : finiteStateMachine.StateTransition("WallState")
-	else : if(playerController.velocity.y >= 0.0) : finiteStateMachine.StateTransition("FallState")
+	if (playerReference.is_on_wall_only() && playerReference.wallDetection.is_colliding() && InputManager.horizontalButton() != 0.0) : finiteStateMachine.changeToState("Wall")
+	elif playerReference.velocity.y >= 0.0 : finiteStateMachine.changeToState("Fall")
 
 func PhysicsUpdate(_delta : float) :
-	if inputManager.jumpButton() && numberOfJumps < maximumJumps :
-		playerController.velocity = Vector2(playerController.Velocity.x, jumpVelocity)
+	if InputManager.jumpButton() && numberOfJumps < maximumJumps :
+		playerReference.velocity = Vector2(playerReference.velocity.x, jumpVelocity)
 		numberOfJumps += 1
