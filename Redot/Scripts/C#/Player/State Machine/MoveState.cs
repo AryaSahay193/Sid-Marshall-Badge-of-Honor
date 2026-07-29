9c262a9cd6c21f2ff6f6641cd2ff6e017717965a
Arya <85712580+AryaSahay193@users.noreply.github.com>
2026-07-29 16:02:24 -0400

    Update MoveState.cs
    Backup before seperating the walk and run logic into two new states

--- Redot/Scripts/C#/Player/State Machine/MoveState.cs
+++ Redot/Scripts/C#/Player/State Machine/MoveState.cs
@@ -1,14 +1,13 @@
 using Godot;
 using System;
 
-public partial class MoveState : ParentState {
+public partial class PlayerMove : BaseState {
 	private float walkingSpeed = 53.0f, maximumSpeed = 164.3f, acceleration = 2.65f, friction = 3.18f;
 	private AudioStreamPlayer grassWalkSFX;
 
 	//Handles code when entering Move-State.
     public override void EnterState() { //Enters the Move-State, code for walking animation and movement.
-		if(playerController.Velocity.X == maximumSpeed * inputManager.horizontalButton()) playerAnimations.Play("Run");
-		else playerAnimations.Play("Walk");
+		playerController.debugText.Text = "[center]State: Move[/center]";
 	}
 
 	//Handles code when exiting Move-State.
@@ -18,35 +17,38 @@ public partial class MoveState : ParentState {
 	}
 
 	public override void UpdateState(float delta) {
-		if(playerController.IsOnFloor()) {
-			if(playerController.Velocity == Vector2.Zero) finiteStateMachine.StateTransition("IdleState"); //Change to Idle State.
-				if(playerController.Velocity.X != 0.0f) {
-				if(inputManager.runButton() && (playerController.Velocity.X > walkingSpeed || playerController.Velocity.X < -walkingSpeed)) playerAnimations.Play("Run");
-				else if(playerController.Velocity.X < walkingSpeed || playerController.Velocity.X > -walkingSpeed) playerAnimations.Play("Walk");
-			}
-		} else if(playerController.Velocity.Y > 0.0f) finiteStateMachine.StateTransition("FallState"); //Change to Fall State.
-		flipCharacter();
+		if(playerController.Velocity.X != 0.0f) {
+			if(inputManager.runPressed()) {
+				if(playerController.Velocity.X == maximumSpeed * inputManager.horizontalDirection()) playerAnimations.Play("Run"); 
+				else playerAnimations.Play("Run_Transition"); 
+			} else playerAnimations.Play("Walk");
+		}
+		
+		if(inputManager.horizontalDirection() == 0.0f) {
+			if(playerController.Velocity.X != 0.0f) {
+				playerAnimations.Play("Skid");
+				playerAnimations.AnimationFinished += () => finiteStateMachine.ChangeToState("IdleState");
+			} else finiteStateMachine.ChangeToState("IdleState"); // Change to Idle State.
+		} else if(playerController.Velocity.Y > 0.0f) finiteStateMachine.ChangeToState("FallState"); // Change to Fall State.
+		playerController.flipCharacter();
 	}
 
 	//Handles code that deals with physics-related movement.
 	public override void PhysicsUpdate(float delta) {
-		if(playerController.IsOnFloorOnly()) {
-			if(inputManager.horizontalButton() != 0.0f) {
-				if(inputManager.runButton()) playerController.Velocity = playerController.Velocity with {X = calculateVelocity(playerController.Velocity.X, maximumSpeed, acceleration)}; //Run-movement code;
-				else playerController.Velocity = playerController.Velocity with {X = calculateVelocity(playerController.Velocity.X, walkingSpeed, acceleration)}; //Walk-movement code.
-			} else if(inputManager.horizontalButton() == 0.0f && playerController.Velocity.X != 0.0f) playerController.Velocity = playerController.Velocity with {X = calculateVelocity(playerController.Velocity.X, 0.0f, friction)}; //Player-friction code.
-		}
-	}
-
-	private float calculateVelocity(float startingVelocity, float endingVelocity, float increment) {
-		float oneAxisSpeed = Mathf.MoveToward(startingVelocity, endingVelocity * inputManager.horizontalButton(), increment);
-		return oneAxisSpeed;
+		if(inputManager.horizontalDirection() != 0.0f) {
+			if(inputManager.runPressed()) playerController.Velocity = playerController.Velocity with {X = calculateVelocity(playerController.Velocity.X, maximumSpeed, acceleration)}; // Run-movement code;
+			else playerController.Velocity = playerController.Velocity with {X = calculateVelocity(playerController.Velocity.X, walkingSpeed, acceleration)}; // Walk-movement code.
+		} else playerController.Velocity = playerController.Velocity with {X = Mathf.MoveToward(playerController.Velocity.X, 0.0f, friction)}; //Player-friction code.
 	}
 
     public override void HandleInput(InputEvent @event) {
 		if(playerController.IsOnFloor()) {
-			if(inputManager.horizontalButton() == 0.0f && playerController.Velocity.X == 0.0f) finiteStateMachine.StateTransition("IdleState"); //Change to Idle State.
-			else if(inputManager.jumpButton()) finiteStateMachine.StateTransition("JumpState"); //Change to Jump State.
+			if(inputManager.jumpPressed()) finiteStateMachine.ChangeToState("JumpState"); // Change to Jump State.
 		}
     }
+
+	private float calculateVelocity(float startingVelocity, float endingVelocity, float increment) {
+		float oneAxisSpeed = Mathf.MoveToward(startingVelocity, endingVelocity * inputManager.horizontalDirection(), increment);
+		return oneAxisSpeed;
+	}
 }
