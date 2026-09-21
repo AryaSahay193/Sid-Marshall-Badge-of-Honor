725eb7edbe8ae3f28a1dccc3018ecffcf04445b7
Arya <85712580+AryaSahay193@users.noreply.github.com>
2026-09-21 17:19:00 -0400

    Update CrouchState.cs
    Most recent state machine for Crouch (from a couple months ago), I will add more stuff in the coming future hopefully

--- Redot/Scripts/C#/Player/State Machine/CrouchState.cs
+++ Redot/Scripts/C#/Player/State Machine/CrouchState.cs
@@ -1,31 +1,45 @@
 using Godot;
 using System;
+using System.Reflection.Metadata.Ecma335;
 
-public partial class CrouchState : ParentState {
+public partial class PlayerCrouch : BaseState {
 	[ExportGroup("Collisions")]
-	[Export] private Shape2D crouchCollision;
-	private float crouchFriction = 1.325f;
+	[Export] private Shape2D normalCollision, crouchCollision;
+
+	private TileMapLayer currentTile;
+	private float crouchFriction = 1.325f, normalHeight, crouchHeight, collisionDifference;
 	private bool isCrouching = false;
 
     public override void EnterState() { 
 		playerController.debugText.Text = "[center]State: Crouch[/center]";
+		// singletonReference.playAnimationOnce(playerAnimations, "Crouch");
+
+		playerAnimations.Play("Crouch");
+		playerAnimations.AnimationFinished += () => playerAnimations.Stop();
 	}
-	
-	public override void UpdateState(float delta) {
-		if(inputManager.crouchButton()) {
-			pauseInputOnAnimation();
-			playerAnimations.Play("Crouch"); //Continuosly plays crouch animation.
+
+	public override void PhysicsUpdate(float delta) { // Handles both physics and collision logic
+		if(playerController.Velocity.X != 0.0f) playerController.Velocity = playerController.Velocity with {X = Mathf.MoveToward(playerController.Velocity.X, 0.0f, crouchFriction)};
+		else playerController.Velocity = playerController.Velocity with {X = 0.0f};
+
+		if(normalCollision is CapsuleShape2D regularCapsule) normalHeight = regularCapsule.Height;
+		if(crouchCollision is CapsuleShape2D crouchCapsule) crouchHeight = crouchCapsule.Height;
+		collisionDifference = (normalHeight - crouchHeight)/2;
+
+		if(!inputManager.crouchPressed()) {
+			playerController.playerCollision.Shape = normalCollision;
+			// playerAnimations.Offset = playerAnimations.Offset with {Y = 0.0f};
+			ExitState(); 
 		} else {
-			SetProcessInput(true);
-			playerAnimations.Play("Crouch_Recover");
-			playerAnimations.AnimationFinished += () => finiteStateMachine.StateTransition("IdleState");
+			//if(inputManager.jumpPressed() && currentTile) finiteStateMachine.ChangeToState("Fall"); // Checks for one-way tile collisions
+			playerController.playerCollision.Shape = crouchCollision;
+			// playerAnimations.Offset = playerAnimations.Offset with {Y = -collisionDifference};
 		}
 	}
 
-	public override void PhysicsUpdate(float delta) {
-		if(playerController.Velocity.X != 0.0f) playerController.Velocity = playerController.Velocity with {X = calculateVelocity(playerController.Velocity.X, 0.0f, crouchFriction)};
-		else playerController.Velocity = playerController.Velocity with {X = 0.0f};
+	public override void ExitState() { 
+		SetProcessInput(true);
+		playerAnimations.Play("Crouch_Recover");
+		playerAnimations.AnimationFinished += () => finiteStateMachine.ChangeToState("IdleState");
 	}
-
-	public override void ExitState() => playerAnimations.Play("Crouch_Recover");
 }
